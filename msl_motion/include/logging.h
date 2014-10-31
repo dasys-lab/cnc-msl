ddb4c60feac7244e74d4693cc69603844515287c
Stephan Opfer <opfer@vs.uni-kassel.de>
2014-10-31 12:26:19 +0100

    motion files

---
+++ msl_motion/include/logging.h
@@ -0,0 +1,21 @@
+#ifndef logging_h
+#define logging_h 1
+
+#include <stdio.h>
+#include <string>
+#include "gonzales.h"
+#include "driver/eposcan.h"
+
+#include <SystemConfig.h>
+#include <Configuration.h>
+
+
+extern gonzales_state gonz_state;
+
+
+void logging_init();
+void log();
+
+
+
+#endif
---
+++ msl_motion/src/logging.c
@@ -0,0 +1,37 @@
+#include "logging.h"
+
+int isLogging = 0;
+std::string logFile;
+FILE *lp;
+extern Controlling::EposCan *ep;
+
+void logging_init() {
+    SystemConfig* sc;
+   	sc = SystemConfig::getInstance();
+
+	Configuration *motion = (*sc)["Motion"];
+	isLogging = motion->get<bool>("Motion","Logging","LogStuff",NULL);
+    if (isLogging) {
+        logFile = motion->get<std::string>("Motion","Logging","LogFile",NULL);
+        lp = fopen(logFile.c_str(),"a");
+        if (lp==NULL) {
+            printf("Cannot Open Log File!\n");
+            exit(-1);
+        }
+    }
+}
+
+void log() {
+    if (isLogging) {
+        fprintf(lp,"%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%f\n",
+            gonz_state.currentPosition.angle,gonz_state.currentPosition.x,gonz_state.currentPosition.y,
+            gonz_state.actualMotion.x,gonz_state.actualMotion.y,gonz_state.actualMotion.rotation,
+            gonz_state.currentMotionGoal.x,gonz_state.currentMotionGoal.y,gonz_state.currentMotionGoal.rotation,
+			ep->ActualRPM(0),ep->ActualRPM(1),ep->ActualRPM(2),ep->ActualRPM(3),
+			ep->DemandRPM(0),ep->DemandRPM(1),ep->DemandRPM(2),ep->DemandRPM(3),
+			ep->ActualCurrent(0),ep->ActualCurrent(1),ep->ActualCurrent(2),ep->ActualCurrent(3),
+			gonz_state.currentSlip
+        );
+
+    }
+}
