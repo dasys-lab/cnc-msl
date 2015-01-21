cd73c9974c1d491b985c3e0a3ad6952a934d08f1
Michael <uk022118@student.uni-kassel.de>
2015-01-21 18:58:36 +0100

    added new qt joystick program

---
+++ msl_keyboard_joystick/src/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file /src/main.cpp
+ *
+ * @brief Qt based gui.
+ *
+ * @date November 2010
+ **/
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui>
+#include <QApplication>
+#include "../include/msl_keyboard_joystick/main_window.hpp"
+
+/*****************************************************************************
+** Main
+*****************************************************************************/
+
+int main(int argc, char **argv) {
+
+    /*********************
+    ** Qt
+    **********************/
+    QApplication app(argc, argv);
+    msl_keyboard_joystick::MainWindow w(argc,argv);
+    w.show();
+    app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
+    int result = app.exec();
+
+	return result;
+}
