825eeba93dd0529cb3a89fc934ac9f342e6654cb
Stephan Opfer <opfer@vs.uni-kassel.de>
2015-02-08 19:31:31 +0100

    initial commit for rqt_msl_refbox plugin

---
+++ rqt_msl_refbox/include/rqt_msl_refbox/RefBox.h
@@ -0,0 +1,40 @@
+#ifndef rqt_msl_refbox__RefBox_H
+#define rqt_msl_refbox__RefBox_H
+
+#include <rqt_gui_cpp/plugin.h>
+
+#include <ui_RefBox.h>
+
+#include <ros/macros.h>
+
+#include <QWidget>
+
+namespace rqt_msl_refbox {
+
+class RefBox
+  : public rqt_gui_cpp::Plugin
+{
+
+  Q_OBJECT
+
+public:
+
+  RefBox();
+
+  virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+
+  virtual void shutdownPlugin();
+
+  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
+
+  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
+
+  Ui::RefBoxWidget ui_;
+
+  QWidget* widget_;
+
+};
+
+}
+
+#endif // rqt_msl_refbox__RefBox_H
--- rqt_msl_refbox/setup.py
+++ rqt_msl_refbox/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rqt_rviz'],
+    packages=['rqt_msl_refbox'],
     package_dir={'': 'src'}
 )
 
