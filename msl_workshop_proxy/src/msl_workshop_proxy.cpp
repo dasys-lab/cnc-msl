c39c9708f2ccb9fae959fbcd8a9d8c449b9c1e6e
Stefan Jakob <stevi872@freenet.de>
2015-02-25 14:52:47 +0100

    bla

--- msl_workshop_proxy/src/msl_workshop_proxy.cpp
+++ msl_workshop_proxy/src/msl_workshop_proxy.cpp
@@ -318,7 +318,7 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "msl_workshop_proxy");
 
 	ros::NodeHandle n;
-	ros::Publisher chatter_pub = n.advertise<msl_sensor_msgs::WorldModelData>("WorldModel/WorldModelData", 1);
+	ros::Publisher chatter_pub = n.advertise<msl_sensor_msgs::WorldModelData>("/WorldModel/WorldModelData", 1);
 
 	ballPub = n.advertise<PointCloud>("/ball", 1);
 	selfPub = n.advertise<PointCloud>("/self", 1);
