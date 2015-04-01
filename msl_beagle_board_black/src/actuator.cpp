3fd6b98c49edb6d846fd9c069efd623dd99ad529
Lukas Will <lukas.will@gmail.com>
2015-04-01 14:10:32 +0200

    bbb

--- msl_beagle_board_black/src/actuator.cpp
+++ msl_beagle_board_black/src/actuator.cpp
@@ -236,9 +236,10 @@ PWM.setRunState(run);
 		value = PWM.getNumericValue();
 		gettimeofday(&nachher, NULL);
 
-		uint32_t diffus = vorher.tv_usec - nachher.tv_usec;
+		uint32_t diffus = static_cast<uint32_t>(vorher.tv_usec) - static_cast<uint32_t>(nachher.tv_usec);
 		uint32_t diffms = TIMEDIFFMS(vorher, nachher);
 
+		std::cout << vorher.tv_usec << " - - " << nachher.tv_usec << std::endl;
 		std::cout << value << " - - " << diffms << " - - " << diffus << std::endl;
 
 
