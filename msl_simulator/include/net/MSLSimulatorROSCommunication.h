bac5b4e967740015ad51cbef84fa39e094cae613
Paul Panin <p.panin@gmx.de>
2015-04-30 14:38:25 +0200

    msl_simulator ssl stuff

---
+++ msl_simulator/include/net/MSLSimulatorROSCommunication.h
@@ -0,0 +1,60 @@
+/*
+ * MSLSimulatorROSCommunication.h
+ *
+ *  Created on: 22.10.2014
+ *      Author: Stephan Opfer
+ */
+
+#ifndef MSLSIMULATORROSCOMMUNICATION_H_
+#define MSLSIMULATORROSCOMMUNICATION_H_
+
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/lock_guard.hpp>
+#include <list>
+#include "ros/ros.h"
+
+#include "msl_simulator/messages_robocup_ssl_wrapper.h"
+#include "msl_simulator/sim_packet.h"
+#include <msl_actuator_msgs/MotionControl.h>
+
+using namespace std;
+
+namespace msl_simulator
+{
+
+	class MSLSimulatorROSCommunication
+	{
+	public:
+		MSLSimulatorROSCommunication();
+		virtual ~MSLSimulatorROSCommunication();
+
+		virtual void tick();
+
+		virtual void send(messages_robocup_ssl_wrapperPtr packet);
+
+		virtual void handleMotionControl(msl_actuator_msgs::MotionControlPtr);
+
+		virtual void startCommunication();
+		virtual void stopCommunication();
+
+		msl_actuator_msgs::MotionControlPtr getSimPacket();
+		bool isQueueEmpty();
+
+		bool isteamyellow = false;
+
+	protected:
+		ros::NodeHandle* rosNode;
+		ros::AsyncSpinner* spinner;
+
+		list<msl_actuator_msgs::MotionControlPtr> recvQueue;
+		boost::mutex mutex;
+
+//		ros::Publisher messagesRoboCupSSLWrapperPublisher;
+		ros::Subscriber motionControl;
+
+		bool isRunning;
+	};
+
+} /* namespace msl_simulator */
+
+#endif /* MSLSIMULATORROSCOMMUNICATION_H_ */
---
+++ msl_simulator/src/net/MSLSimulatorROSCommunication.cpp
@@ -0,0 +1,108 @@
+/*
+ * MSLSimulatorROSCommunication.cpp
+ *
+ *  Created on: 22.10.2014
+ *      Author: Stephan Opfer
+ */
+
+#include "net/MSLSimulatorROSCommunication.h"
+
+#include <ros/node_handle.h>
+#include <ros/publisher.h>
+#include <ros/subscriber.h>
+#include <msl_actuator_msgs/MotionControl.h>
+#include <string.h>
+
+
+namespace msl_simulator
+{
+
+	MSLSimulatorROSCommunication::MSLSimulatorROSCommunication()
+	{
+		this->isRunning = false;
+		rosNode = new ros::NodeHandle();
+		spinner = new ros::AsyncSpinner(4);
+
+//		messagesRoboCupSSLWrapperPublisher = rosNode->advertise<messages_robocup_ssl_wrapper>("/MSLSimulator/MessagesRoboCupSSLWrapper", 2);
+
+//		commandSubscriber = rosNode->subscribe("/MSLSimulator/SimPacket", 10, &MSLSimulatorROSCommunication::handleSimPacket, (MSLSimulatorROSCommunication*)this);
+
+		motionControl = rosNode->subscribe("/MotionControl", 10, &MSLSimulatorROSCommunication::handleMotionControl, (MSLSimulatorROSCommunication*)this);
+
+	}
+
+	MSLSimulatorROSCommunication::~MSLSimulatorROSCommunication()
+	{
+		if (this->isRunning)
+		{
+			spinner->stop();
+		}
+		delete spinner;
+
+//		messagesRoboCupSSLWrapperPublisher.shutdown();
+		rosNode->shutdown();
+		delete rosNode;
+
+	}
+
+	void MSLSimulatorROSCommunication::tick()
+	{
+		if (this->isRunning)
+		{
+			ros::spinOnce();
+		}
+	}
+
+	void MSLSimulatorROSCommunication::send(messages_robocup_ssl_wrapperPtr packet)
+	{
+//		messagesRoboCupSSLWrapperPublisher.publish(packet);
+	}
+
+	void MSLSimulatorROSCommunication::handleMotionControl(msl_actuator_msgs::MotionControlPtr motion)
+	{
+		boost::lock_guard<boost::mutex> lock(mutex);
+		recvQueue.push_back(motion);
+	}
+
+	msl_actuator_msgs::MotionControlPtr MSLSimulatorROSCommunication::getSimPacket()
+	{
+		boost::lock_guard<boost::mutex> lock(mutex);
+		msl_actuator_msgs::MotionControlPtr motion = recvQueue.front();
+
+		msl_simulator::sim_packetPtr packet;
+//		msl_simulator::sim_robot_commandPtr robotCommand;
+//
+//		packet->commands.isteamyellow = this->isteamyellow;
+//		packet->commands.timestamp = ros::Time::now();
+//
+//		robotCommand->id = motion->senderID;
+//		robotCommand->veltangent = motion->motion.rotation;
+//		robotCommand->velnormal = motion->motion.translation;
+//		robotCommand->velangular = motion->motion.angle;
+
+
+
+//		packet->commands.robot_commands.push_back(robotCommand);
+
+		recvQueue.pop_front();
+		return motion;
+	}
+
+	bool MSLSimulatorROSCommunication::isQueueEmpty()
+	{
+		boost::lock_guard<boost::mutex> lock(mutex);
+		return recvQueue.empty();
+	}
+
+	void MSLSimulatorROSCommunication::startCommunication()
+	{
+		this->isRunning = true;
+		spinner->start();
+	}
+	void MSLSimulatorROSCommunication::stopCommunication()
+	{
+		this->isRunning = false;
+		spinner->stop();
+	}
+
+} /* namespace alicaRosProxy */
