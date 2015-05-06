5801dc5fe8352848d96381c6adb5afe65c906dc3
Tobias <Tobi_80@gmx.net>
2015-05-06 18:17:03 +0200

    carpet calibrator

---
+++ carpet_calibrator/src/CarpetCalibrator.cpp
@@ -0,0 +1,16 @@
+/*
+ * CarpetCalibrator.cpp
+ *
+ *  Created on: 06.05.2015
+ *      Author: tobi
+ */
+
+namespace msl_vision
+{
+
+} /* namespace msl_vision */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
