856bb657f362cfbf1d7c54f184c6821b80ec00ac
Lukas Will <lukas.will@gmail.com>
2015-05-07 18:19:31 +0200

    BBB Lightbarrier

--- msl_beagle_board_black/src/actuator.cpp
+++ msl_beagle_board_black/src/actuator.cpp
@@ -202,7 +202,7 @@ int main(int argc, char** argv) {
 	// ROS Init
 	ros::init(argc, argv, "ActuatorController");
 	ros::NodeHandle node;
-	ros::Rate loop_rate(1);		// in Hz
+	ros::Rate loop_rate(30);		// in Hz
 
 	ros::Subscriber sscSub = node.subscribe<msl_actuator_msgs::ShovelSelectCmd>("ShovelSelectControl", 25, handleShovelSelectControl);
 	ros::Subscriber mlcSub = node.subscribe<msl_actuator_msgs::MotionLight>("CNActuator/MotionLight", 25, handleMotionLight);
