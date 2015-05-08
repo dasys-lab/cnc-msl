193e9de9891a577e6b357e91f6177a7c4827bfe3
CNBasement <cn@das-lab.net>
2015-05-08 13:48:39 +0200

    just a test

--- msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
@@ -102,7 +102,7 @@ namespace alica
         }
         for(get =container.begin(); get != container.end(); get++)
         {
-        	std::cout<< get << std::endl;
+        	std::cout<< *get << std::endl;
         }
 
 
