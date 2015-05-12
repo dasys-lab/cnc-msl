128602ed5bb3138d3b32666e7a9e28e50fa98694
CNBasement <cn@das-lab.net>
2015-05-12 15:18:31 +0200

    just a test

--- msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
@@ -136,8 +136,8 @@ namespace alica
         right=arithmeticAverage;
 */
 
-        left =  rodo->motion.translation * (1.0 / 40.0) ;
-        right = rodo->motion.translation * (1.0 / 40.0) ;
+       // left =  rodo->motion.translation * (1.0 / 40.0) ;
+        //right = rodo->motion.translation * (1.0 / 40.0) ;
 
         bhc.leftMotor = max(min(left, 60), -60);
         bhc.rightMotor = max(min(right, 60), -60);
