3d4b220e0adcac1f8cb8399355a3953f7d440891
CNBasement <cn@das-lab.net>
2015-05-19 18:28:33 +0200

    just a test

--- msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
@@ -64,8 +64,8 @@ namespace alica
         	x=pow(cos(wm->rawSensorData.getOwnVelocityMotion()->angle),2);
         	y=pow(sin(wm->rawSensorData.getOwnVelocityMotion()->angle),2);
 
-        	left=arithmeticAverage*(x+y);
-        	right=arithmeticAverage*(x-y);
+        	left=arithmeticAverage*(x+y)/10;
+        	right=arithmeticAverage*(x-y)/10;
 
         //HintenRechts
         if(wm->rawSensorData.getOwnVelocityMotion()->angle>0 && wm->rawSensorData.getOwnVelocityMotion()->angle<M_PI/2)
@@ -73,8 +73,8 @@ namespace alica
         	x=pow(cos(wm->rawSensorData.getOwnVelocityMotion()->angle),2);
         	y=pow(sin(wm->rawSensorData.getOwnVelocityMotion()->angle),2);
 
-        	left=arithmeticAverage*(x+y);
-        	right=arithmeticAverage*(x-y);
+        	left=arithmeticAverage*(x+y)/10;
+        	right=arithmeticAverage*(x-y)/10;
 
 
         if(wm->rawSensorData.getOwnVelocityMotion()->angle < 0)
@@ -87,8 +87,8 @@ namespace alica
         	y=-pow(cos(wm->rawSensorData.getOwnVelocityMotion()->angle),2);
         	x=-pow(sin(wm->rawSensorData.getOwnVelocityMotion()->angle),2);
 
-        	left=arithmeticAverage*(y-x);
-        	right=arithmeticAverage*(y+x);
+        	left=arithmeticAverage*(y-x)/10;
+        	right=arithmeticAverage*(y+x)/10;
 
         	//VorneLinks
         if(wm->rawSensorData.getOwnVelocityMotion()->angle>=M_PI/2 && wm->rawSensorData.getOwnVelocityMotion()->angle<=M_PI)
@@ -96,8 +96,8 @@ namespace alica
         	 y=pow(cos(wm->rawSensorData.getOwnVelocityMotion()->angle),2);
         	 x=pow(sin(wm->rawSensorData.getOwnVelocityMotion()->angle),2);
 
-        	 left=arithmeticAverage*(y-x);
-        	 right=arithmeticAverage*(y+x);
+        	 left=arithmeticAverage*(y-x)/10;
+        	 right=arithmeticAverage*(y+x)/10;
 
 
 
