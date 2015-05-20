48cc0009346bb5f67b86f712cba20a6c16db33e5
CNBasement <cn@das-lab.net>
2015-05-20 16:43:08 +0200

    tweaking and cleanup AttackOpp

--- msl_expressions/autogenerated/src/Plans/Behaviours/AttackOpp.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/AttackOpp.cpp
@@ -7,98 +7,95 @@ using namespace std;
 /*PROTECTED REGION END*/
 namespace alica
 {
-    /*PROTECTED REGION ID(staticVars1430324527403) ENABLED START*/ //initialise static variables here
-    /*PROTECTED REGION END*/
-    AttackOpp::AttackOpp() :
-            DomainBehaviour("AttackOpp")
-    {
-        /*PROTECTED REGION ID(con1430324527403) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    AttackOpp::~AttackOpp()
-    {
-        /*PROTECTED REGION ID(dcon1430324527403) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    void AttackOpp::run(void* msg)
-    {
-        /*PROTECTED REGION ID(run1430324527403) ENABLED START*/
+	/*PROTECTED REGION ID(staticVars1430324527403) ENABLED START*/ //initialise static variables here
+	/*PROTECTED REGION END*/
+	AttackOpp::AttackOpp() :
+			DomainBehaviour("AttackOpp")
+	{
+		/*PROTECTED REGION ID(con1430324527403) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
+	AttackOpp::~AttackOpp()
+	{
+		/*PROTECTED REGION ID(dcon1430324527403) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
+	void AttackOpp::run(void* msg)
+	{
+		/*PROTECTED REGION ID(run1430324527403) ENABLED START*/
 
-        auto me = wm->rawSensorData.getOwnPositionVision();
+		auto me = wm->rawSensorData.getOwnPositionVision();
 
-        auto egoBallPos = wm->ball.getEgoBallPosition();
+		auto egoBallPos = wm->ball.getEgoBallPosition();
 
-        auto obstacles = wm->robots.getObstacles();
-        if (me == nullptr || egoBallPos == nullptr || obstacles == nullptr)
-        {
-            cerr << "insufficient information for AttackOpp" << endl;
-            return;
-        }
+		//auto obstacles = wm->robots.getObstacles();
 
-        for (auto obstacle : *obstacles)
-        {
-            // TODO: Get closest obstacle to ball
-        }
+		//for (auto obstacle : *obstacles)
+		//{
+		// TODO: Get closest obstacle to ball
+		//}
 
-        if (!me.operator bool())
-        {
-            return;
-        }
+		if (me == nullptr || egoBallPos == nullptr)
+		{
+			cerr << "insufficient information for AttackOpp" << endl;
+			return;
+		}
 
-        //auto egoTarget = alloTarget.alloToEgo(*me);
+		if (!me.operator bool())
+		{
+			return;
+		}
 
-        msl_actuator_msgs::MotionControl mc;
+		//auto egoTarget = alloTarget.alloToEgo(*me);
 
-        if (egoBallPos != nullptr)
-        {
-            mc = RobotMovement::moveToPointCarefully(egoBallPos, egoBallPos, 0);
+		msl_actuator_msgs::MotionControl mc;
 
-            double summe = 0.0;
-            static double olddistance = 0.0;
+		mc = RobotMovement::moveToPointCarefully(egoBallPos, egoBallPos, 300);
 
-            const double Kp = 1;
-            const double Ki = 0.0;
-            const double Kd = 1;
+		double summe = 0.0;
+		static double olddistance = 0.0;
 
-            //distance ball to robot
-            double distance = egoBallPos->length();
+		const double Kp = 1;
+		const double Ki = 0.0;
+		const double Kd = 1;
 
-            summe = summe + distance;
-            double movement = Kp * distance + Ki * summe + Kd * (distance - olddistance);
-            olddistance = distance;
+		//distance ball to robot
+		double distance = egoBallPos->length();
 
-            cout << "movement: " << movement << endl;
-            cout << "distance: " << distance << endl;
-            // mc.motion.translation =
-        }
-        else
-        {
-            mc = RobotMovement::moveToPointCarefully(egoBallPos, make_shared < msl::CNPoint2D > (0.0, 0.0), 0);
-        }
+		summe = summe + distance;
+		double movement = Kp * distance + Ki * summe + Kd * (distance - olddistance);
+		olddistance = distance;
 
-        if (egoBallPos->length() < 250)
-        {
-            this->success = true;
-        }
-        /*
-         // TODO: Prüfen ob Wert korrekt ist
-         auto radius_own = sqrt(pow((me->x - ballPos->x), 2) + pow(me->y - ballPos->y, 2));
-         std::cout << "Eigener Radius zum Ball: " << radius_own << std::endl;
+		cout << "movement: " << movement << endl;
+		cout << "distance: " << distance << endl;
 
-         auto radius_distance_ball = 600;
+		// translation = 1000 => 1 m/s
+		// mc.motion.translation =
 
-         // TODO: Schnittpunkt berechnen
-         */
-        send(mc);
 
-        //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    void AttackOpp::initialiseParameters()
-    {
-        /*PROTECTED REGION ID(initialiseParameters1430324527403) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
+	if (egoBallPos->length() < 250)
+	{
+		this->success = true;
+	}
+	/*
+	 // TODO: Prüfen ob Wert korrekt ist
+	 auto radius_own = sqrt(pow((me->x - ballPos->x), 2) + pow(me->y - ballPos->y, 2));
+	 std::cout << "Eigener Radius zum Ball: " << radius_own << std::endl;
+
+	 auto radius_distance_ball = 600;
+
+	 // TODO: Schnittpunkt berechnen
+	 */
+	send (mc);
+
+//Add additional options here
+/*PROTECTED REGION END*/
+}
+void AttackOpp::initialiseParameters()
+{
+	/*PROTECTED REGION ID(initialiseParameters1430324527403) ENABLED START*/ //Add additional options here
+	/*PROTECTED REGION END*/
+}
 /*PROTECTED REGION ID(methods1430324527403) ENABLED START*/ //Add additional methods here
 /*PROTECTED REGION END*/
 } /* namespace alica */
