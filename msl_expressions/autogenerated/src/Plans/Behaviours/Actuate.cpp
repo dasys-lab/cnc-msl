de7e193efc9f4ac5b0579e7eeb9721e90f09b653
CNBasement <cn@das-lab.net>
2015-05-22 17:24:18 +0200

    just a test

--- msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
@@ -3,123 +3,74 @@ using namespace std;
 
 /*PROTECTED REGION ID(inccpp1417017518918) ENABLED START*/ //Add additional includes here
 #include "math.h"
+
 /*PROTECTED REGION END*/
 namespace alica
 {
-    /*PROTECTED REGION ID(staticVars1417017518918) ENABLED START*/ //initialise static variables here
-    /*PROTECTED REGION END*/
-    Actuate::Actuate() :
-            DomainBehaviour("Actuate")
-    {
-        /*PROTECTED REGION ID(con1417017518918) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    Actuate::~Actuate()
-    {
-        /*PROTECTED REGION ID(dcon1417017518918) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    void Actuate::run(void* msg)
-    {
-        /*PROTECTED REGION ID(run1417017518918) ENABLED START*/ //Add additional options here
-        msl_actuator_msgs::BallHandleCmd bhc;
-        auto rodo = wm->rawSensorData.getOwnVelocityMotion();
-        int left, right;
-        // TODO x und y wahrscheinlich durch merge verloren gegangen, nochmal anschauen
-        double x, y;
+	/*PROTECTED REGION ID(staticVars1417017518918) ENABLED START*/ //initialise static variables here
+	/*PROTECTED REGION END*/
+	Actuate::Actuate() :
+			DomainBehaviour("Actuate")
+	{
+		/*PROTECTED REGION ID(con1417017518918) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
+	Actuate::~Actuate()
+	{
+		/*PROTECTED REGION ID(dcon1417017518918) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
+	void Actuate::run(void* msg)
+	{
+		/*PROTECTED REGION ID(run1417017518918) ENABLED START*/ //Add additional options here
+		msl_actuator_msgs::BallHandleCmd bhc;
+		auto rodo = wm->rawSensorData.getOwnVelocityMotion();
 
-        if (rodo == nullptr)
-        {
-            cout << "Actuate RODO is empty help" << endl;
-            return;
-        }
+		int left, right;
+		// TODO x und y wahrscheinlich durch merge verloren gegangen, nochmal anschauen
+		double x, y;
 
-        //QualityOfService= wm->rawSensorData.getOpticalFlow()->qos;
-        // x Werte richtig vertauscht
+		if (rodo == nullptr)
+		{
+			cout << "Actuate RODO is empty help" << endl;
+			return;
+		}
 
-        //Mittelwert der Aktuellen 4 Werte
+		double summe = 0.0;
+		static double olddistance = 0.0;
 
-        double arithmeticAverage = 0;
-        double newParamer = wm->rawSensorData.getOwnVelocityMotion()->translation;
-        list<double>::iterator parameter;
+		const double Kp = 2.0;
+		const double Ki = 0.0;
+		const double Kd = 1.7;
+		double qualityOfService =wm->rawSensorData.getOpticalFlowQoS();
 
-        if (arithmeticAverageBox.size() == 4)
-        {
-            arithmeticAverageBox.pop_back();
-        }
 
-        arithmeticAverageBox.push_front(newParamer);
 
-        for (parameter = arithmeticAverageBox.begin(); parameter != arithmeticAverageBox.end(); parameter++)
-        {
-            arithmeticAverage += *parameter;
-        }
+		summe = summe + qualityOfService;
+		double movement = Kp * qualityOfService + Ki * summe + Kd * (qualityOfService - olddistance);
+		olddistance = qualityOfService;
 
-        arithmeticAverage = arithmeticAverage / 8;
+		left=olddistance;
+		right=olddistance;
+		cout << "QualityOfService: " << wm->rawSensorData.getOpticalFlowQoS() << endl;
+		cout << "olddistance : "<<olddistance<<endl;
 
-        //Vorsteuerung
-        //VorneRechts
-        if (wm->rawSensorData.getOwnVelocityMotion()->angle <= M_PI
-                && wm->rawSensorData.getOwnVelocityMotion()->angle >= M_PI / 2)
 
-            x = pow(cos(wm->rawSensorData.getOwnVelocityMotion()->angle), 2);
-        y = pow(sin(wm->rawSensorData.getOwnVelocityMotion()->angle), 2);
 
-        left = arithmeticAverage * (x + y) / 10;
-        right = arithmeticAverage * (x - y) / 10;
+		// left =  rodo->motion.translation * (1.0 / 40.0) ;
+		//right = rodo->motion.translation * (1.0 / 40.0) ;
 
-        //HintenRechts
-        if (wm->rawSensorData.getOwnVelocityMotion()->angle > 0
-                && wm->rawSensorData.getOwnVelocityMotion()->angle < M_PI / 2)
+		bhc.leftMotor = max(min(left, 60), -60);
+		bhc.rightMotor = max(min(right, 60), -60);
 
-            x = pow(cos(wm->rawSensorData.getOwnVelocityMotion()->angle), 2);
-        y = pow(sin(wm->rawSensorData.getOwnVelocityMotion()->angle), 2);
-
-        left = arithmeticAverage * (x + y) / 10;
-        right = arithmeticAverage * (x - y) / 10;
-
-        if (wm->rawSensorData.getOwnVelocityMotion()->angle < 0)
-        {
-            wm->rawSensorData.getOwnVelocityMotion()->angle = wm->rawSensorData.getOwnVelocityMotion()->angle + M_PI;
-
-            //HintenLinks
-            if (wm->rawSensorData.getOwnVelocityMotion()->angle >= M_PI / 2
-                    && wm->rawSensorData.getOwnVelocityMotion()->angle <= M_PI)
-
-                y = -pow(cos(wm->rawSensorData.getOwnVelocityMotion()->angle), 2);
-            x = -pow(sin(wm->rawSensorData.getOwnVelocityMotion()->angle), 2);
-
-            left = arithmeticAverage * (x - y) / 10;
-            right = arithmeticAverage * (x + y) / 10;
-
-            //VorneLinks
-            if (wm->rawSensorData.getOwnVelocityMotion()->angle >= M_PI / 2
-                    && wm->rawSensorData.getOwnVelocityMotion()->angle <= M_PI)
-
-                y = pow(cos(wm->rawSensorData.getOwnVelocityMotion()->angle), 2);
-            x = pow(sin(wm->rawSensorData.getOwnVelocityMotion()->angle), 2);
-
-            left = arithmeticAverage * (x - y) / 10;
-            right = arithmeticAverage * (y + x) / 10;
-
-        }
-        //left=arithmeticAverage;
-        //right=arithmeticAverage;
-
-        // left =  rodo->motion.translation * (1.0 / 40.0) ;
-        //right = rodo->motion.translation * (1.0 / 40.0) ;
-
-        bhc.leftMotor = max(min(left, 60), -60);
-        bhc.rightMotor = max(min(right, 60), -60);
-
-        this->send(bhc);
-        /*PROTECTED REGION END*/
-    }
-    void Actuate::initialiseParameters()
-    {
-        /*PROTECTED REGION ID(initialiseParameters1417017518918) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
+		this->send(bhc);
+		/*PROTECTED REGION END*/
+	}
+	void Actuate::initialiseParameters()
+	{
+		/*PROTECTED REGION ID(initialiseParameters1417017518918) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
 /*PROTECTED REGION ID(methods1417017518918) ENABLED START*/ //Add additional methods here
 /*PROTECTED REGION END*/
 } /* namespace alica */
