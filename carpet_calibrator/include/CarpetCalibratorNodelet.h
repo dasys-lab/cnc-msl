7f7d81434cc5c563a985d10bee138cd2af380bcd
Carpe Noctem <cn@das-lab.net>
2015-05-27 19:06:40 +0200

    new stuff for carpet calculator

---
+++ carpet_calibrator/include/CarpetCalibratorNodelet.h
@@ -0,0 +1,47 @@
+/*
+ * CarpetCalibratorNodelet.h
+ *
+ *  Created on: May 20, 2015
+ *      Author: cn
+ */
+
+#ifndef CNC_MSLDRIVER_CARPET_CALIBRATOR_SRC_CARPETCALIBRATORNODELET_H_
+#define CNC_MSLDRIVER_CARPET_CALIBRATOR_SRC_CARPETCALIBRATORNODELET_H_
+
+#include <nodelet/nodelet.h>
+#include <ros/package.h>
+#include <ros/ros.h>
+#include <boost/thread.hpp>
+#include <pluginlib/class_list_macros.h>
+#include <image_transport/image_transport.h>
+
+
+using namespace std;
+
+namespace msl_vision
+{
+	typedef struct {
+		uint8_t u;
+		uint8_t y1;
+		uint8_t v;
+		uint8_t y2;
+	} YUV422DoublePixel;
+	class CarpetCalibratorNodelet : public nodelet::Nodelet
+	{
+	public:
+		 CarpetCalibratorNodelet();
+		 ~CarpetCalibratorNodelet();
+	     virtual void onInit();
+	     void run();
+	     void imageCb(const sensor_msgs::ImageConstPtr& image_msg,
+	     				                             const sensor_msgs::CameraInfoConstPtr& info_msg);
+	     void connectCb();
+	private:
+	     boost::shared_ptr<image_transport::ImageTransport> it_;
+	     image_transport::CameraSubscriber sub_camera_;
+		 ros::NodeHandle nh;
+		 boost::shared_ptr<boost::thread> deviceThread_;
+	};
+
+}
+#endif /* CNC_MSLDRIVER_CARPET_CALIBRATOR_SRC_CARPETCALIBRATORNODELET_H_ */
---
+++ carpet_calibrator/src/CarpetCalibratorNodelet.cpp
@@ -0,0 +1,102 @@
+/*
+ * CarpetCalibratorNodelet.cpp
+ *
+ *  Created on: May 20, 2015
+ *      Author: cn
+ */
+
+#include <string>
+#include "CarpetCalibratorNodelet.h"
+#include <pluginlib/class_list_macros.h>
+#include <nodelet/nodelet.h>
+#include <boost/thread.hpp>
+#include <signal.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv.h>
+
+//nodelet manager starten:
+//rosrun nodelet nodelet manager __name:=nodelet_manager
+//
+//unser nodelet starten
+//rosrun nodelet nodelet load carpet_calibrator/CarpetCalibratorNodelet nodelet_manager __name:=nodelet1
+//
+//starten von camera1394
+//rosrun nodelet nodelet standalone camera1394/driver
+namespace msl_vision
+{
+
+	CarpetCalibratorNodelet::CarpetCalibratorNodelet()
+	{
+	}
+
+	CarpetCalibratorNodelet::~CarpetCalibratorNodelet()
+	{
+	}
+
+	void CarpetCalibratorNodelet::imageCb(const sensor_msgs::ImageConstPtr& image_msg,
+				                             const sensor_msgs::CameraInfoConstPtr& info_msg) {
+		const cv::Mat image = cv_bridge::toCvShare(image_msg)->image;
+		YUV422DoublePixel* pColor = (YUV422DoublePixel*) image.data;
+		cv::Mat rgbImage;
+		cv::Mat image2rgb(image_msg->height, image_msg->width, CV_8UC1);
+		cv::Mat image2rgb1(image_msg->height, image_msg->width, CV_8UC1);
+		cv::Mat image2rgb2(image_msg->height, image_msg->width, CV_8UC1);
+		int counter = 0;
+		int size = info_msg->height * info_msg->width;
+		for(int i = 0; i < (size); i++) {
+			image2rgb.data[i] = image.data[i*3];
+			image2rgb1.data[i] = image.data[i*3+1];
+			image2rgb2.data[i] = image.data[i*3+2];
+			/*image2rgb.data[counter++] = pColor[i].y1;
+			image2rgb.data[counter++] = pColor[i].v;
+			image2rgb.data[counter++] = pColor[i].u;
+
+			image2rgb.data[counter++] = pColor[i].y2;
+			image2rgb.data[counter++] = pColor[i].v;
+			image2rgb.data[counter++] = pColor[i].u;*/
+		}
+
+		//cv::cvtColor(image2rgb, rgbImage, CV_YUV2BGR); //
+		cv::imshow("lol", image2rgb);
+		cv::imshow("lol1", image2rgb1);
+		cv::imshow("lol2", image2rgb2);
+		cv::waitKey(1);
+		cout << "Image Callback, size: " << image.size.p[1] << endl;
+
+
+	}
+
+	void CarpetCalibratorNodelet::onInit()
+	{
+		NODELET_INFO("CarpetCalibrator: Initialize Nodelet");
+
+		const vector<string> argv = this->getMyArgv();
+		nh = getNodeHandle();
+
+		it_.reset(new image_transport::ImageTransport(nh));
+
+		int argc = argv.size();
+
+		std::string yaml_filename;
+
+		deviceThread_ = boost::shared_ptr< boost::thread >
+		    (new boost::thread(boost::bind(&msl_vision::CarpetCalibratorNodelet::run, this)));
+
+		image_transport::TransportHints hints("raw", ros::TransportHints(), getNodeHandle());
+		sub_camera_ = it_->subscribeCamera("camera/image_raw",1, &CarpetCalibratorNodelet::imageCb,this,  hints);
+	}
+
+	void CarpetCalibratorNodelet::run()
+	{
+		cout << " carpet blubb"  << endl;
+	}
+
+
+}
+
+PLUGINLIB_DECLARE_CLASS(carpet_calibrator, msl_vision::CarpetCalibratorNodelet,
+						msl_vision::CarpetCalibratorNodelet, nodelet::Nodelet);
+
