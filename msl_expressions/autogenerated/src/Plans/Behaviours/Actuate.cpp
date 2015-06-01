c4946689b30ed91375d2c8a214846915b8cfd689
CNBasement <cn@das-lab.net>
2015-06-01 15:09:07 +0200

    just a test

--- msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
@@ -59,7 +59,7 @@ namespace alica
 
 		 const double KiLeft = 0.0;
 		 const double KdLeft = 0.0;
-		 const double KpLeft = 1.0;
+		 const double KpLeft = 0.5;
 		 const double SollwertLeft = 90;
 
 
@@ -95,7 +95,7 @@ namespace alica
 
 		 const double KiRight = 0.0;
 		 const double KdRight = 0.0;
-		 const double KpRight = 1.0;
+		 const double KpRight = 0.5;
 		 const double SollwertRight = 90;
 
 
