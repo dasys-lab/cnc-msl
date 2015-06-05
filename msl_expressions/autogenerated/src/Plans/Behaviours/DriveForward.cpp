aa955d4dbb8742383963bec1e5b07b3e9540c4e8
nase <info@das-lab.net>
2015-06-05 17:29:49 +0200

    test

--- msl_expressions/autogenerated/src/Plans/Behaviours/DriveForward.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/DriveForward.cpp
@@ -29,7 +29,7 @@ namespace alica
 
         mc.motion.angle =M_PI;
    	mc.motion.rotation = 0.0;
-        mc.motion.translation =1500.0;
+        mc.motion.translation =2000.0;
 
 
         this->send(mc);
