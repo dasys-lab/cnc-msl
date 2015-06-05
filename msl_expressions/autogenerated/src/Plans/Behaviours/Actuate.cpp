b0d673b82e240cbcf335c8cecccde01c41299a65
CNBasement <cn@das-lab.net>
2015-06-05 18:20:46 +0200

    test

--- msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/Actuate.cpp
@@ -124,16 +124,8 @@ namespace alica
 		feedForwardLeft = max(min(lefty, 1.0), -1.2);
 		KvLeft = (feedForwardRight*eFunktion*arithmeticAverage);
 */
-		KvRight=-1*(arithmeticAverage*(1/10));
-		KvLeft=-1*(arithmeticAverage*(1/10));
-
-
-
-
-
-
-
-
+		KvRight=-1*(arithmeticAverage*(1/10)+5);
+		KvLeft=-1*(arithmeticAverage*(1/10)+5);
 
 
 		/*
