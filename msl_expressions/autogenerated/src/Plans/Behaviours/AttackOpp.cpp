18963748b1b9dcac3cf489b439763a67b6790854
CNBasement <cn@das-lab.net>
2015-07-24 12:58:37 +0200

    fgjf

--- msl_expressions/autogenerated/src/Plans/Behaviours/AttackOpp.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/AttackOpp.cpp
@@ -69,13 +69,13 @@ namespace alica
 				isMovingAwayIter++;
 				isMovingCloserIter = 0;
 			}
-			if (isMovingCloserIter >= maxIter || egoBallVelocity->length() < 250)
+			if (isMovingCloserIter >= maxIter)
 			{
 				cout << "get closer" << endl;
 				mc = ballGetsCloser(me, egoBallVelocity, egoBallPos);
 
 			}
-			else if (isMovingAwayIter >= maxIter)
+			else if (isMovingAwayIter >= maxIter || egoBallVelocity->length() < 250)
 			{
 				cout << "roll away" << endl;
 				mc = driveToMovingBall(egoBallPos, egoBallVelocity);
