02f0f62135d6706676460b25b1092df476d5ad0b
Stefan Jakob <s.jakob872@gmail.com>
2015-07-28 15:23:35 +0200

    removed cout

--- msl_worldmodel/src/Game.cpp
+++ msl_worldmodel/src/Game.cpp
@@ -90,7 +90,6 @@ namespace msl
 	//TODO handle all situations
 	void Game::onRefBoxCommand(msl_msgs::RefBoxCommandPtr msg)
 	{
-		cout << "onRefBoxCommand" << endl;
 		// Put the referee box command into the ringbuffer
 
 		/*
@@ -101,15 +100,12 @@ namespace msl
 		shared_ptr<msl_msgs::RefBoxCommand> cmd = shared_ptr<msl_msgs::RefBoxCommand>(
 				msg.get(), [msg](msl_msgs::RefBoxCommand*) mutable
 				{	msg.reset();});
-		cout << "onRefBoxCommand2" << endl;
 		shared_ptr<InformationElement<msl_msgs::RefBoxCommand>> refBoxCmd = make_shared<
 				InformationElement<msl_msgs::RefBoxCommand>>(cmd, wm->getTime());
 		refBoxCmd->certainty = 1.0;
 		refBoxCommand.add(refBoxCmd);
-		cout << "onRefBoxCommand3" << endl;
 		// Set the current refbox situation
 		lock_guard<mutex> lock(refereeMutex);
-		cout << "onRefBoxCommand4" << endl;
 		switch (msg->cmd)
 		{
 			case msl_msgs::RefBoxCommand::START:
@@ -117,7 +113,6 @@ namespace msl
 				{
 					timeSinceStart = wm->getTime();
 				}
-				cout << "Situation: Start" << endl;
 				situation = Situation::Start;
 				break;
 			case msl_msgs::RefBoxCommand::STOP:
@@ -326,7 +321,6 @@ namespace msl
 	bool Game::checkSituation(Situation situation)
 	{
 		lock_guard<mutex> lock(situationChecker);
-		cout << "check: " << this->situation << "|" << situation << endl;
 		return this->situation == situation;
 	}
 
