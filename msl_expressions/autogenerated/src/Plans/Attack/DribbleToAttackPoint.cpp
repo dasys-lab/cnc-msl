966ac111572161861ad6dac20fb24df8f966c533
Stefan Jakob <s.jakob872@gmail.com>
2015-07-29 10:53:08 +0200

    tuning

--- msl_expressions/autogenerated/src/Plans/Attack/DribbleToAttackPoint.cpp
+++ msl_expressions/autogenerated/src/Plans/Attack/DribbleToAttackPoint.cpp
@@ -134,7 +134,7 @@ namespace alica
 
 		// add the motion towards the ball
 		double maxDribbleSpeed = 2000;
-		driveTo = driveTo + temp->normalize() * max(maxDribbleSpeed, temp->length()) * 1.5;
+		driveTo = driveTo + temp->normalize() * min(maxDribbleSpeed, temp->length()) * 1.25;
 
 		mc.motion.angle = driveTo->angleTo();
 		mc.motion.translation = min(this->maxVel, driveTo->length());
