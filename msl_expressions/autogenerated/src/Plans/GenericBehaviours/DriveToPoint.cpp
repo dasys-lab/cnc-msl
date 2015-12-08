e51a18a05bfb4852923eb94bbb0da60a9f04bc12
cn Basemant <cn@das-lab.net>
2015-12-08 15:18:06 +0100

    revert changes

--- msl_expressions/autogenerated/src/Plans/GenericBehaviours/DriveToPoint.cpp
+++ msl_expressions/autogenerated/src/Plans/GenericBehaviours/DriveToPoint.cpp
@@ -39,7 +39,6 @@ namespace alica
          else
          {*/
         mc = RobotMovement::moveToPointCarefully(egoTarget, make_shared < geometry::CNPoint2D > (-1000.0, 0.0), 0);
-        mc.motion.translation = 200;
         //}
 
         if (egoTarget->length() < 250)
