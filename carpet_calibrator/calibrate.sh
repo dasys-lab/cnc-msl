dc6044cd6c373a7662d85370757b7cbf02332e82
Tobias <Tobi_80@gmx.net>
2016-01-23 16:44:33 +0100

    accept with enter

--- carpet_calibrator/calibrate.sh
+++ carpet_calibrator/calibrate.sh
@@ -2,17 +2,17 @@
 roslaunch carpet_calibrator start.launch
 
 echo "Is there an *.raw file in $DOMAIN_CONFIG_FOLDER/$HOSTNAME?"
-read -r -p "[y/n]" response
+read -r -p "[Y/N]" response
 response=${response,,} # tolower
 
-if [[ $response =~ ^(yes|y| ) ]]; then
+if [[ $response =~ ^(yes|y|"") ]]; then
 	echo "open image..."
 	eog $DOMAIN_CONFIG_FOLDER/$HOSTNAME/CarpetCalibImage.png&
 
 	echo "Does this image look good?"
-	read -r -p "[y/n]" response
+	read -r -p "[Y/N]" response
 	response=${response,,} # tolower
-	if [[ $response =~ ^(yes|y| ) ]]; then
+	if [[ $response =~ ^(yes|y|"") ]]; then
 		killall eog
 		rosrun carpet_calibrator carpet_calibrator_line_points
 		rosrun msl_vision msl_carpet_calculator
