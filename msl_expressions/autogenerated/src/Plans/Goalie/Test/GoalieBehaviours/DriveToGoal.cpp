54982ac4626540f0d54a9e840e6b4e70ec7b7e0f
Thomas Falk <tfalkster@gmail.com>
2016-01-27 15:39:20 +0100

    Goalie now watches ball between goal posts

--- msl_expressions/autogenerated/src/Plans/Goalie/Test/GoalieBehaviours/DriveToGoal.cpp
+++ msl_expressions/autogenerated/src/Plans/Goalie/Test/GoalieBehaviours/DriveToGoal.cpp
@@ -6,66 +6,61 @@ using namespace std;
 /*PROTECTED REGION END*/
 namespace alica
 {
-    /*PROTECTED REGION ID(staticVars1447863424939) ENABLED START*/ //initialise static variables here
-    /*PROTECTED REGION END*/
-    DriveToGoal::DriveToGoal() :
-            DomainBehaviour("DriveToGoal")
-    {
-        /*PROTECTED REGION ID(con1447863424939) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    DriveToGoal::~DriveToGoal()
-    {
-        /*PROTECTED REGION ID(dcon1447863424939) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    void DriveToGoal::run(void* msg)
-    {
-        /*PROTECTED REGION ID(run1447863424939) ENABLED START*/ //Add additional options here
-<<<<<<< HEAD
-        cout << "### DriveToGoal ##" << endl;
-=======
-        shared_ptr < geometry::CNPosition > me = wm->rawSensorData.getOwnPositionVision();
-        if (me == nullptr)
-        {
-            return;
-        }
->>>>>>> 897ba8acbda3dad27447f4ab08472d5168819832
+	/*PROTECTED REGION ID(staticVars1447863424939) ENABLED START*/ //initialise static variables here
+	/*PROTECTED REGION END*/
+	DriveToGoal::DriveToGoal() :
+			DomainBehaviour("DriveToGoal")
+	{
+		/*PROTECTED REGION ID(con1447863424939) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
+	DriveToGoal::~DriveToGoal()
+	{
+		/*PROTECTED REGION ID(dcon1447863424939) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
+	void DriveToGoal::run(void* msg)
+	{
+		/*PROTECTED REGION ID(run1447863424939) ENABLED START*/ //Add additional options here
+		shared_ptr<geometry::CNPosition> me = wm->rawSensorData.getOwnPositionVision();
+		if (me == nullptr)
+		{
+			return;
+		}
 
-        shared_ptr < geometry::CNPosition > me = wm->rawSensorData.getOwnPositionVision();
-        msl_actuator_msgs::MotionControl mc;
-        if (me == nullptr)
-        {
-            mc.motion.angle = 0;
-            mc.motion.rotation = 0;
-            mc.motion.translation = 0;
+		msl_actuator_msgs::MotionControl mc;
+		if (me == nullptr)
+		{
+			mc.motion.angle = 0;
+			mc.motion.rotation = 0;
+			mc.motion.translation = 0;
 
-            cout << " Stop!" << endl;
-            cout << "### DriveToGoal ###\n" << endl;
-        }
-        else
-        {
+			cout << " Stop!" << endl;
+			cout << "### DriveToGoal ###\n" << endl;
+		}
+		else
+		{
 
-            double egoX = MSLFootballField::posOwnGoalMid()->alloToEgo(*me)->x;
-            double egoY = MSLFootballField::posOwnGoalMid()->alloToEgo(*me)->y;
-            shared_ptr < geometry::CNPoint2D > fieldCenterTarget = MSLFootballField::posCenterMarker()->alloToEgo(*me);
+			double egoX = MSLFootballField::posOwnGoalMid()->alloToEgo(*me)->x;
+			double egoY = MSLFootballField::posOwnGoalMid()->alloToEgo(*me)->y;
+			shared_ptr<geometry::CNPoint2D> fieldCenterTarget = MSLFootballField::posCenterMarker()->alloToEgo(*me);
 
-            cout << " Driving to goal" << endl;
-            //mc = RobotMovement::moveToPointFast(make_shared < geometry::CNPoint2D > (egoX - 100, egoY), fieldCenterTarget,
-            //                                  100, 0);
-            mc = RobotMovement::moveToPointCarefully(make_shared < geometry::CNPoint2D > (egoX - 100, egoY),
-                                                     fieldCenterTarget, 100, 0);
-            cout << "### DriveToGoal ###\n" << endl;
-        }
-        send(mc);
+			cout << " Driving to goal" << endl;
+			//mc = RobotMovement::moveToPointFast(make_shared < geometry::CNPoint2D > (egoX - 100, egoY), fieldCenterTarget,
+			//                                  100, 0);
+			mc = RobotMovement::moveToPointCarefully(make_shared<geometry::CNPoint2D>(egoX - 100, egoY),
+														fieldCenterTarget, 100, 0);
+			cout << "### DriveToGoal ###\n" << endl;
+		}
+		send(mc);
 
-        /*PROTECTED REGION END*/
-    }
-    void DriveToGoal::initialiseParameters()
-    {
-        /*PROTECTED REGION ID(initialiseParameters1447863424939) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
+		/*PROTECTED REGION END*/
+	}
+	void DriveToGoal::initialiseParameters()
+	{
+		/*PROTECTED REGION ID(initialiseParameters1447863424939) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
 /*PROTECTED REGION ID(methods1447863424939) ENABLED START*/ //Add additional methods here
 /*PROTECTED REGION END*/
 } /* namespace alica */
--- msl_expressions/autogenerated/src/Plans/Goalie/Test/GoalieBehaviours/WatchBall.cpp
+++ msl_expressions/autogenerated/src/Plans/Goalie/Test/GoalieBehaviours/WatchBall.cpp
@@ -7,7 +7,6 @@ using namespace std;
 /*PROTECTED REGION END*/
 namespace alica
 {
-<<<<<<< HEAD
 	/*PROTECTED REGION ID(staticVars1447863466691) ENABLED START*/ //initialise static variables here
 	/*PROTECTED REGION END*/
 	WatchBall::WatchBall() :
@@ -51,8 +50,9 @@ namespace alica
 			targetY = rightGoalPost - puffer;
 		}
 
-		shared_ptr<geometry::CNPosition> egoTarget = make_shared<geometry::CNPoint2D>(targetX, targetY);
+		auto egoTarget = make_shared < geometry::CNPoint2D > (targetX, targetY);
 		mc = RobotMovement::moveToPointFast(egoTarget, goalMid, 100, 0);
+		//mc = RobotMovement::moveToPointFast(egoTarget, goalMid, 100, 0);
 		cout << "### WatchBall ###\n" << endl;
 
 		send(mc);
@@ -63,50 +63,6 @@ namespace alica
 		/*PROTECTED REGION ID(initialiseParameters1447863466691) ENABLED START*/ //Add additional options here
 		/*PROTECTED REGION END*/
 	}
-=======
-    /*PROTECTED REGION ID(staticVars1447863466691) ENABLED START*/ //initialise static variables here
-    /*PROTECTED REGION END*/
-    WatchBall::WatchBall() :
-            DomainBehaviour("WatchBall")
-    {
-        /*PROTECTED REGION ID(con1447863466691) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    WatchBall::~WatchBall()
-    {
-        /*PROTECTED REGION ID(dcon1447863466691) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    void WatchBall::run(void* msg)
-    {
-        /*PROTECTED REGION ID(run1447863466691) ENABLED START*/ //Add additional options here
-        msl_actuator_msgs::MotionControl mc;
-
-        shared_ptr < geometry::CNPoint2D > egoBallPos = wm->ball.getEgoBallPosition();
-        shared_ptr < geometry::CNPoint2D > goalMid = MSLFootballField::posOwnGoalMid();
-        shared_ptr < geometry::CNPosition > me = wm->rawSensorData.getOwnPositionVision();
-
-        double targetX = goalMid->alloToEgo(*me)->x - 100;
-        double targetY = egoBallPos->y;
-        double penaltyLength = MSLFootballField::PenaltyAreaLength;
-        double goalWidth = MSLFootballField::GoalAreaWidth;
-
-        // TODO: Set y-borders, armlength away from goal posts and let goalie stop once reached
-
-        //cout << "targetX: " << targetX << endl;
-        //cout << "targetY: " << targetY<< endl;
-
-        mc = RobotMovement::moveToPointFast(make_shared < geometry::CNPoint2D > (targetX, targetY), goalMid, 100, 0);
-        send(mc);
-
-        /*PROTECTED REGION END*/
-    }
-    void WatchBall::initialiseParameters()
-    {
-        /*PROTECTED REGION ID(initialiseParameters1447863466691) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
->>>>>>> 897ba8acbda3dad27447f4ab08472d5168819832
 /*PROTECTED REGION ID(methods1447863466691) ENABLED START*/ //Add additional methods here
 /*PROTECTED REGION END*/
 } /* namespace alica */
