7b7aadba23f083a9522884a278d4e32ea0b657f8
Thomas Falk <tfalkster@gmail.com>
2016-01-27 14:59:31 +0100

    goalie watches ball between goal posts

--- msl_expressions/autogenerated/src/Plans/Goalie/Test/GoalieBehaviours/WatchBall.cpp
+++ msl_expressions/autogenerated/src/Plans/Goalie/Test/GoalieBehaviours/WatchBall.cpp
@@ -7,48 +7,61 @@ using namespace std;
 /*PROTECTED REGION END*/
 namespace alica
 {
-    /*PROTECTED REGION ID(staticVars1447863466691) ENABLED START*/ //initialise static variables here
-    /*PROTECTED REGION END*/
-    WatchBall::WatchBall() :
-            DomainBehaviour("WatchBall")
-    {
-        /*PROTECTED REGION ID(con1447863466691) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    WatchBall::~WatchBall()
-    {
-        /*PROTECTED REGION ID(dcon1447863466691) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
-    void WatchBall::run(void* msg)
-    {
-        /*PROTECTED REGION ID(run1447863466691) ENABLED START*/ //Add additional options here
-    	msl_actuator_msgs::MotionControl mc;
+	/*PROTECTED REGION ID(staticVars1447863466691) ENABLED START*/ //initialise static variables here
+	/*PROTECTED REGION END*/
+	WatchBall::WatchBall() :
+			DomainBehaviour("WatchBall")
+	{
+		/*PROTECTED REGION ID(con1447863466691) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
+	WatchBall::~WatchBall()
+	{
+		/*PROTECTED REGION ID(dcon1447863466691) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
+	void WatchBall::run(void* msg)
+	{
+		/*PROTECTED REGION ID(run1447863466691) ENABLED START*/ //Add additional options here
+		cout << "### WatchBall ###" << endl;
 
-    	shared_ptr < geometry::CNPoint2D> egoBallPos = wm->ball.getEgoBallPosition();
-    	shared_ptr < geometry::CNPoint2D > goalMid = MSLFootballField::posOwnGoalMid();
-        shared_ptr < geometry::CNPosition> me = wm->rawSensorData.getOwnPositionVision();
+		shared_ptr<geometry::CNPosition> me = wm->rawSensorData.getOwnPositionVision();
+		shared_ptr<geometry::CNPoint2D> goalMid = MSLFootballField::posOwnGoalMid();
+		msl_actuator_msgs::MotionControl mc;
 
-        double targetX = goalMid->alloToEgo(*me) -> x - 100;
-        double targetY = egoBallPos -> y;
-        double penaltyLength = MSLFootballField::PenaltyAreaLength;
-        double goalWidth = MSLFootballField::GoalAreaWidth;
+		double targetX = MSLFootballField::posOwnGoalMid()->egoToAllo(*me)->x - 100;
+		double targetY = wm->ball.getEgoBallPosition()->y;
 
-        // TODO: Set y-borders, armlength away from goal posts and let goalie stop once reached
+		cout << " Watching ball" << endl;
+		double leftGoalPost = MSLFootballField::posLeftOwnGoalPost()->alloToEgo(*me)->y;
+		double rightGoalPost = MSLFootballField::posRightOwnGoalPost()->alloToEgo(*me)->y;
 
-        //cout << "targetX: " << targetX << endl;
-        //cout << "targetY: " << targetY<< endl;
+		// TODO: armlength when extended and balldiameter/2
+		double puffer = 100 + 200;
 
-        mc = RobotMovement::moveToPointFast(make_shared < geometry::CNPoint2D > (targetX, targetY), goalMid, 100, 0);
-        send(mc);
+		if (targetY < leftGoalPost)
+		{
+			cout << "  - y: " << targetY << endl;
+			targetY = leftGoalPost + puffer;
+		}
+		else if (targetY > rightGoalPost)
+		{
+			cout << "  - y: " << targetY << endl;
+			targetY = rightGoalPost - puffer;
+		}
 
-        /*PROTECTED REGION END*/
-    }
-    void WatchBall::initialiseParameters()
-    {
-        /*PROTECTED REGION ID(initialiseParameters1447863466691) ENABLED START*/ //Add additional options here
-        /*PROTECTED REGION END*/
-    }
+		shared_ptr<geometry::CNPosition> egoTarget = make_shared<geometry::CNPoint2D>(targetX, targetY);
+		mc = RobotMovement::moveToPointFast(egoTarget, goalMid, 100, 0);
+		cout << "### WatchBall ###\n" << endl;
+
+		send(mc);
+		/*PROTECTED REGION END*/
+	}
+	void WatchBall::initialiseParameters()
+	{
+		/*PROTECTED REGION ID(initialiseParameters1447863466691) ENABLED START*/ //Add additional options here
+		/*PROTECTED REGION END*/
+	}
 /*PROTECTED REGION ID(methods1447863466691) ENABLED START*/ //Add additional methods here
 /*PROTECTED REGION END*/
 } /* namespace alica */
