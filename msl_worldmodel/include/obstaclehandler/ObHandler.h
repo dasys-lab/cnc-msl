bdae90da3ca4f6a9b37f6f93103fecc5be6f941d
Stefan Jakob <s.jakob872@gmail.com>
2016-02-11 14:43:41 +0100

    working on merged opps

---
+++ msl_worldmodel/include/obstaclehandler/ObHandler.h
@@ -0,0 +1,23 @@
+/*
+ * ObHandler.h
+ *
+ *  Created on: Feb 11, 2016
+ *      Author: Stefan Jakob
+ */
+
+#ifndef CNC_MSL_MSL_WORLDMODEL_SRC_OBSTACLEHANDLER_OBHANDLER_H_
+#define CNC_MSL_MSL_WORLDMODEL_SRC_OBSTACLEHANDLER_OBHANDLER_H_
+
+namespace msl
+{
+
+	class ObHandler
+	{
+	public:
+		ObHandler();
+		virtual ~ObHandler();
+	};
+
+} /* namespace msl */
+
+#endif /* CNC_MSL_MSL_WORLDMODEL_SRC_OBSTACLEHANDLER_OBHANDLER_H_ */
---
+++ msl_worldmodel/src/obstaclehandler/ObHandler.cpp
@@ -0,0 +1,24 @@
+/*
+ * ObHandler.cpp
+ *
+ *  Created on: Feb 11, 2016
+ *      Author: Stefan Jakob
+ */
+
+#include "obstaclehandler/ObHandler.h"
+
+namespace msl
+{
+
+	ObHandler::ObHandler()
+	{
+		// TODO Auto-generated constructor stub
+
+	}
+
+	ObHandler::~ObHandler()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+} /* namespace msl */
