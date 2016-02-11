0ce0a25e2b9a8274c0b22fe1651d391c1aba6e05
cn Basemant <cn@das-lab.net>
2016-02-11 12:13:11 +0100

    test rotation sign

--- msl_expressions/autogenerated/src/Plans/Behaviours/CalcCalib.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/CalcCalib.cpp
@@ -27,6 +27,8 @@ namespace alica
             calibPosVisionY = this->wm->rawSensorData.getOwnPositionVision(0)->y;
         }
 
+        auto calibRotation = this->wm->rawSensorData.getOwnVelocityMotion(0)->rotation;
+
         calibPosMotionX = this->wm->rawSensorData.getOwnPositionMotion(0)->x;
         calibPosMotionY = this->wm->rawSensorData.getOwnPositionMotion(0)->y;
 
@@ -67,8 +69,25 @@ namespace alica
 
         tempyoyo++;
 
+        static int minusCounter = 0;
+        static int plusCounter = 0;
+
+        if (calibRotation > 0)
+        {
+
+        	plusCounter++;
+        }
+        else
+        {
+
+        	minusCounter++;
+        }
+
         std::cout << "correctedWayX : " << correctedPosX << std::endl;
         std::cout << "correctedWayY : " << correctedPosY << std::endl;
+        std::cout << "rotation : " << calibRotation << std::endl;
+        std::cout << "plusCounter : " << plusCounter << std::endl;
+        std::cout << "minusCounter : " << minusCounter << std::endl;
         std::cout << "" << std::endl;
 
         //msl_actuator_msgs::MotionControl mc;
