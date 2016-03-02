069684ef7ce21bb16b35741b37851d3c5ed7502f
Brain2 <cn@das-lab.net>
2016-03-02 17:10:16 +0100

    adapt RobotTest

--- msl_expressions/autogenerated/src/Plans/Robotcheck/RobotTest.cpp
+++ msl_expressions/autogenerated/src/Plans/Robotcheck/RobotTest.cpp
@@ -4,348 +4,290 @@ using namespace std;
 /*PROTECTED REGION ID(inccpp1456756113767) ENABLED START*/ //Add additional includes here
 #include "robotmovement/RobotMovement.h"
 /*PROTECTED REGION END*/
-namespace alica
-{
-	/*PROTECTED REGION ID(staticVars1456756113767) ENABLED START*/ //initialise static variables here
+namespace alica {
+/*PROTECTED REGION ID(staticVars1456756113767) ENABLED START*/ //initialise static variables here
+/*PROTECTED REGION END*/
+RobotTest::RobotTest() :
+		DomainBehaviour("RobotTest") {
+	/*PROTECTED REGION ID(con1456756113767) ENABLED START*/ //Add additional options here
 	/*PROTECTED REGION END*/
-	RobotTest::RobotTest() :
-			DomainBehaviour("RobotTest")
-	{
-		/*PROTECTED REGION ID(con1456756113767) ENABLED START*/ //Add additional options here
-		/*PROTECTED REGION END*/
+}
+RobotTest::~RobotTest() {
+	/*PROTECTED REGION ID(dcon1456756113767) ENABLED START*/ //Add additional options here
+	/*PROTECTED REGION END*/
+}
+void RobotTest::run(void* msg) {
+	/*PROTECTED REGION ID(run1456756113767) ENABLED START*/ //Add additional options here
+	auto me = wm->rawSensorData.getOwnPositionVision();
+
+	// testing motion ================================================================
+
+	if (driveForward) {
+		cout << "drive forward!" << endl;
+		driveForward = translationRotationRobot(300, true, 3000);
+		if (!driveForward) {
+			driveBack = true;
+		}
 	}
-	RobotTest::~RobotTest()
-	{
-		/*PROTECTED REGION ID(dcon1456756113767) ENABLED START*/ //Add additional options here
-		/*PROTECTED REGION END*/
+	if (driveBack) {
+		cout << "drive back!" << endl;
+		driveBack = translationRotationRobot(-300, true, 3000);
+		if (!driveBack) {
+			rotateForward = true;
+		}
 	}
-	void RobotTest::run(void* msg)
-	{
-		/*PROTECTED REGION ID(run1456756113767) ENABLED START*/ //Add additional options here
-		auto me = wm->rawSensorData.getOwnPositionVision();
-
-		// testing motion ================================================================
-
-		if (driveForward)
-		{
-			cout << "drive forward!" << endl;
-			driveForward = translationRotationRobot(300, true, 3000);
-			if (!driveForward)
-			{
-				driveBack = true;
-			}
+	if (rotateForward) {
+		cout << "rotate right!" << endl;
+		rotateForward = translationRotationRobot(2, false, 3000);
+		if (!rotateForward) {
+			rotateBack = true;
 		}
-		if (driveBack)
-		{
-			cout << "drive back!" << endl;
-			driveBack = translationRotationRobot(-300, true, 3000);
-			if (!driveBack)
-			{
-				rotateForward = true;
-			}
+	}
+	if (rotateBack) {
+		cout << "rotate left!" << endl;
+		rotateBack = translationRotationRobot(-2, false, 3000);
+		if (!rotateBack) {
+			actuatorForward = true;
 		}
-		if (rotateForward)
-		{
-			cout << "rotate right!" << endl;
-			rotateForward = translationRotationRobot(5, false, 3000);
-			if (!rotateForward)
-			{
-				rotateBack = true;
-			}
+	}
+
+	// testing actuator ==============================================================
+
+	if (actuatorForward) {
+		cout << " rotate actuator forward" << endl;
+		actuatorForward = actuatorRobot(3000, 30);
+		if (!actuatorForward) {
+			actuatorBack = true;
 		}
-		if (rotateBack)
-		{
-			cout << "rotate left!" << endl;
-			rotateBack = translationRotationRobot(-5, false, 3000);
-			if (!rotateForward)
-			{
-				kicker = true;
-			}
+	}
+	if (actuatorBack) {
+		cout << " rotate actuator back" << endl;
+		actuatorBack = actuatorRobot(3000, -30);
+		if (!actuatorBack) {
+			lightBarrier = true;
 		}
+	}
 
-		// testing kicker ================================================================
+	// light barrier ==============================================================
 
-		if (kicker)
-		{
-			cout << "kicking = 200" << endl;
-			kicker = kickerRobot(200);
-			if (!kicker)
-			{
-				actuatorForward = true;
-			}
+	if (lightBarrier) {
+		cout << "testing light barrier" << endl;
+		lightBarrier = lightBarrierRobot();
+		if (!lightBarrier) {
+			opticalFlow = true;
 		}
+	}
 
-		// testing actuator ==============================================================
+	// optical flow ===============================================================
 
-		if (actuatorForward)
-		{
-			cout << " rotate actuator forward" << endl;
-			actuatorForward = actuatorRobot(3000, 30);
-			if (!actuatorForward)
-			{
-				actuatorBack = true;
-			}
+	if (opticalFlow) {
+		cout << "testing optical flow" << endl;
+		opticalFlow = opticalFlowRobot();
+		if (!opticalFlow) {
+			imu = true;
 		}
-		if (actuatorBack)
-		{
-			cout << " rotate actuator back" << endl;
-			actuatorBack = actuatorRobot(3000, -30);
-			if (!actuatorBack)
-			{
-				lightBarrier = true;
-			}
+	}
+
+	// IMU ========================================================================
+
+	if (imu) {
+		cout << "testing IMU" << endl;
+		imu = imuRobot();
+		if (!imu) {
+			shovelSelectLow = true;
 		}
+	}
 
-		// light barrier ==============================================================
+	// shovel select ==============================================================
 
-		if (lightBarrier)
-		{
-			cout << "testing light barrier" << endl;
-			lightBarrier = lightBarrierRobot();
-			if (!lightBarrier)
-			{
-				opticalFlow = true;
-			}
+	if (shovelSelectLow) {
+		cout << "testing shovelSelectLow" << endl;
+		shovelSelectLow = shovelSelectRobot(true, 3000);
+		if (!shovelSelectLow) {
+			shovelSelectHigh = true;
 		}
+	}
 
-		// optical flow ===============================================================
-
-		if (opticalFlow)
+	if (shovelSelectHigh) {
+		cout << "testing shovelSelectHigh" << endl;
+		shovelSelectHigh = shovelSelectRobot(true, 3000);
+		if (!shovelSelectHigh)
 		{
-			cout << "testing optical flow" << endl;
-			opticalFlow = opticalFlowRobot();
-			if (!opticalFlow)
-			{
-				imu = true;
-			}
+			kicker = true;
 		}
+	}
 
-		// IMU ========================================================================
+	// testing kicker ================================================================
 
-		if (imu)
-		{
-			cout << "testing IMU" << endl;
-			imu = imuRobot();
-			if (!imu)
-			{
-				shovelSelectLow = true;
-			}
+	if (kicker) {
+		cout << "kicking = 200" << endl;
+		kicker = kickerRobot(300);
+//		if (!kicker) {
+//			actuatorForward = true;
+//		}
+	}
+
+	if (!driveForward && !driveBack && !rotateBack && !rotateForward && !kicker
+			&& !actuatorForward && !actuatorBack && !opticalFlow && !imu
+			&& !shovelSelectLow && !shovelSelectHigh) {
+		cout << "finished testing" << endl;
+		cout
+				<< "This robot check behavior was presented by very fast and often working Michael Gottesleben and Lukas Will!"
+				<< endl;
+		cout << "Have fun bitches i'm out!\n" << endl;
+		printGlasses();
+		this->success = true;
+	}
+	/*PROTECTED REGION END*/
+}
+void RobotTest::initialiseParameters() {
+	/*PROTECTED REGION ID(initialiseParameters1456756113767) ENABLED START*/ //Add additional options here
+	move = 0;
+
+	driveForward = true;
+	driveBack = false;
+	rotateBack = false;
+	rotateForward = false;
+	kicker = false;
+	actuatorForward = false;
+	actuatorBack = false;
+	opticalFlow = false;
+	imu = false;
+	shovelSelectLow = false;
+	shovelSelectHigh = false;
+	lightBarrier = false;
+
+	/*PROTECTED REGION END*/
+}
+/*PROTECTED REGION ID(methods1456756113767) ENABLED START*/ //Add additional methods here
+bool RobotTest::translationRotationRobot(int movement, bool trans,
+		int duration) {
+	msl_actuator_msgs::MotionControl mc;
+	cout << "move = " << move << endl;
+	if (trans) {
+		if (move < (30 * duration) / 1000 && trans) {
+			mc.motion.translation = movement;
+		} else {
+			move = 0;
+			return false;
 		}
-
-		// shovel select ==============================================================
-
-		if (shovelSelectLow)
-		{
-			cout << "testing shovelSelectLow" << endl;
-			shovelSelectLow = shovelSelectRobot(true, 3000);
-			if (!shovelSelectLow)
-			{
-				shovelSelectHigh = true;
-			}
+	} else {
+		if (move < (30 * duration) / 1000 && !trans) {
+			mc.motion.rotation = movement;
+		} else {
+			move = 0;
+			return false;
 		}
+	}
+	move++;
+	send(mc);
 
-		if (shovelSelectHigh)
-		{
-			cout << "testing shovelSelectHigh" << endl;
-			shovelSelectHigh = shovelSelectRobot(true, 3000);
+	return true;
+}
 
-		}
+bool RobotTest::kickerRobot(int power) {
+	msl_actuator_msgs::KickControl kc;
+	kc.enabled = true;
+	if (power > 0) {
+		kc.power = power;
+		send(kc);
+		return false;
 
-		if (!driveForward && !driveBack && !rotateBack && !rotateForward && !kicker && !actuatorForward && !actuatorBack
-				&& !opticalFlow && !imu && !shovelSelectLow && !shovelSelectHigh)
-		{
-			cout << "finished testing" << endl;
-			cout
-					<< "This robot check behavior was presented by very fast and often working Michael Gottesleben and Lukas Will!"
+	} else {
+		cerr << "Kick power may not be negative!" << endl;
+		kc.power = 0;
+	}
+	return true;
+}
+
+bool RobotTest::actuatorRobot(int duration, int power) {
+	msl_actuator_msgs::BallHandleCmd bhc;
+	if (move < (30 * duration) / 1000) {
+		if (power < 100 && power > -100) {
+			bhc.leftMotor = power;
+			bhc.rightMotor = power;
+		} else {
+			bhc.leftMotor = 0;
+			bhc.rightMotor = 0;
+			cerr << "Rotation speed may only be in range of -100 to 100!"
 					<< endl;
-			cout << "Have fun bitches i'm out!\n" << endl;
-			printGlasses();
-			this->success = true;
 		}
-		/*PROTECTED REGION END*/
-	}
-	void RobotTest::initialiseParameters()
-	{
-		/*PROTECTED REGION ID(initialiseParameters1456756113767) ENABLED START*/ //Add additional options here
+	} else {
 		move = 0;
-
-		driveForward = true;
-		driveBack = false;
-		rotateBack = false;
-		rotateForward = false;
-		kicker = false;
-		actuatorForward = false;
-		actuatorBack = false;
-		opticalFlow = false;
-		imu = false;
-		shovelSelectLow = false;
-		shovelSelectHigh = false;
-		lightBarrier = false;
-
-		/*PROTECTED REGION END*/
-	}
-	/*PROTECTED REGION ID(methods1456756113767) ENABLED START*/ //Add additional methods here
-	bool RobotTest::translationRotationRobot(int movement, bool trans, int duration)
-	{
-		msl_actuator_msgs::MotionControl mc;
-		cout << "move = " << move << endl;
-		if (trans)
-		{
-			if (move < (30 * duration) / 1000 && trans)
-			{
-				mc.motion.translation = movement;
-			}
-			else
-			{
-				move = 0;
-				return false;
-			}
-		}
-		else
-		{
-			if (move < (30 * duration) / 1000 && !trans)
-			{
-				mc.motion.rotation = movement;
-			}
-			else
-			{
-				move = 0;
-				return false;
-			}
-		}
-		move++;
-		send(mc);
-
-		return true;
+		return false;
 	}
 
-	bool RobotTest::kickerRobot(int power)
-	{
-		msl_actuator_msgs::KickControl kc;
-		kc.enabled = true;
-		if (power > 0)
-		{
-			kc.power = power;
-			send(kc);
-			return false;
+	move++;
+	bhc.enabled = true;
+	send(bhc);
+	return true;
+}
 
-		}
-		else
-		{
-			cerr << "Kick power may not be negative!" << endl;
-			kc.power = 0;
-		}
-		return true;
-	}
+bool RobotTest::lightBarrierRobot() {
+	auto lbi = wm->rawSensorData.getLightBarrier();
 
-	bool RobotTest::actuatorRobot(int duration, int power)
-	{
-		msl_actuator_msgs::BallHandleCmd bhc;
-		if (move < (30 * duration) / 1000)
-		{
-			if (power < 100 && power > -100)
-			{
-				bhc.leftMotor = power;
-				bhc.rightMotor = power;
-			}
-			else
-			{
-				bhc.leftMotor = 0;
-				bhc.rightMotor = 0;
-				cerr << "Rotation speed may only be in range of -100 to 100!" << endl;
-			}
+	if (lbi != nullptr) {
+		auto static lb_old = *lbi;
+		auto lb_new = *lbi;
+
+		if (lb_old != lb_new) {
+			lb_old = lb_new;
+			cout << "light barrier = " << lb_old << endl;
+			move++;
 		}
-		else
-		{
+
+		if (move > 5) {
 			move = 0;
+			cout << "light barrier is working!" << endl;
 			return false;
 		}
-
-		move++;
-		bhc.enabled = true;
-		send(bhc);
-		return true;
-	}
-
-	bool RobotTest::lightBarrierRobot()
-	{
-		auto lbi = wm->rawSensorData.getLightBarrier();
-
-		if (lbi != nullptr)
-		{
-			auto static lb_old = *lbi;
-			auto lb_new = *lbi;
-
-			if (lb_old != lb_new)
-			{
-				lb_old = lb_new;
-				cout << "light barrier = " << lb_old << endl;
-				move++;
-			}
-
-			if (move > 5)
-			{
-				move = 0;
-				cout << "light barrier is working!" << endl;
-				return false;
-			}
-		}
-		else
-		{
-			cerr << "no data from light barrier!" << endl;
-			return false;
-		}
+	} else {
+		cerr << "no data from light barrier!" << endl;
 		return false;
 	}
+	return false;
+}
 
-	bool RobotTest::opticalFlowRobot()
-	{
-		// for testing
+bool RobotTest::opticalFlowRobot() {
+	// for testing
+	return false;
+	auto of = wm->rawSensorData.getOpticalFlow();
+	if (of != nullptr) {
 		return false;
-		auto of = wm->rawSensorData.getOpticalFlow();
-		if (of != nullptr)
-		{
-			return false;
-		}
-		else
-		{
-			cerr << "No data from optical flow!" << endl;
-			return false;
-		}
-		return true;
-	}
-
-	bool RobotTest::imuRobot()
-	{
-		cout << "IMU currently not included in world model" << endl;
+	} else {
+		cerr << "No data from optical flow!" << endl;
 		return false;
 	}
+	return true;
+}
 
-	bool RobotTest::shovelSelectRobot(bool pass, int duration)
-	{
-		msl_actuator_msgs::ShovelSelectCmd sc;
-		if (move < (30 * duration) / 1000)
-		{
-			sc.passing = pass;
-			send(sc);
-		}
-		else
-		{
-			move = 0;
-			return false;
-		}
-		move++;
-		return true;
-	}
+bool RobotTest::imuRobot() {
+	cout << "IMU currently not included in world model" << endl;
+	return false;
+}
 
-	void RobotTest::printGlasses()
-	{
-		cout << "   IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII\n "
-				<< "IIII              IIIIIII   III   IIIIIIIIIIIII   IIIIII   III   IIIIIIIIIIIII\n"
-				<< "                     IIIIIII   III   IIIIIIIIII      IIIIII   III   IIIIIIII\n"
-				<< "                        IIIIIII   III   IIII            IIIIII   III   III\n"
-				<< "                          IIIIIIIIIIIIIII                IIIIIIIIIIIIIII\n" << endl;
+bool RobotTest::shovelSelectRobot(bool pass, int duration) {
+	msl_actuator_msgs::ShovelSelectCmd sc;
+	if (move < (30 * duration) / 1000) {
+		sc.passing = pass;
+		send(sc);
+	} else {
+		move = 0;
+		return false;
 	}
+	move++;
+	return true;
+}
+
+void RobotTest::printGlasses() {
+	cout
+			<< "   IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII\n "
+			<< "IIII              IIIIIII   III   IIIIIIIIIIIII   IIIIII   III   IIIIIIIIIIIII\n"
+			<< "                     IIIIIII   III   IIIIIIIIII      IIIIII   III   IIIIIIII\n"
+			<< "                        IIIIIII   III   IIII            IIIIII   III   III\n"
+			<< "                          IIIIIIIIIIIIIII                IIIIIIIIIIIIIII\n"
+			<< endl;
+}
 /*PROTECTED REGION END*/
 } /* namespace alica */
 
