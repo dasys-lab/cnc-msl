e1d2948689bee19e8044ca1e3bd8c3927480a6f2
Carpe Noctem <cn@das-lab.net>
2016-03-09 16:00:05 +0100

    add in genereicexec passmsg and set the blocker/defender

--- msl_expressions/autogenerated/src/Plans/GenericBehaviours/CheckGoalKick.cpp
+++ msl_expressions/autogenerated/src/Plans/GenericBehaviours/CheckGoalKick.cpp
@@ -42,12 +42,12 @@ namespace alica
 
         cout << "==========================================================================" << endl;
         if (hitPoint)
-        	cout << "hits the goal: " << hitPoint->x << ", " << hitPoint->y << ", " << hitPoint->z << endl;
+            cout << "hits the goal: " << hitPoint->x << ", " << hitPoint->y << ", " << hitPoint->z << endl;
         else
-        	cout << "hits the goal: false" << endl;
+            cout << "hits the goal: false" << endl;
 
         if (false == hitPoint)
-        	return;
+            return;
 
         bool checkGoalKeeperResult = checkGoalKeeper(hitPoint);
         bool checkShootPossiblilityResult = checkShootPossibility(hitPoint);
@@ -57,13 +57,13 @@ namespace alica
             kicking (hitPoint);
         }
 
-		cout << "check goal keeper: " << (checkGoalKeeperResult ? "true" : "false") << endl;
-		cout << "check shoot possibility: " << (checkShootPossiblilityResult ? "true" : "false") << endl;
-		cout << "kick power: " << cout_kickpower << endl;
-		cout << "kicking = " << cout_kicking << endl;
+        cout << "check goal keeper: " << (checkGoalKeeperResult ? "true" : "false") << endl;
+        cout << "check shoot possibility: " << (checkShootPossiblilityResult ? "true" : "false") << endl;
+        cout << "kick power: " << cout_kickpower << endl;
+        cout << "kicking = " << cout_kicking << endl;
 
-		cout_kicking = false;
-		cout_kickpower = 0;
+        cout_kicking = false;
+        cout_kickpower = 0;
 
         /*PROTECTED REGION END*/
     }
@@ -177,7 +177,7 @@ namespace alica
 
             if (abs(obs->alloToEgo(*ownPos)->y) < minOppYDist)
             {
-            	std::cout << "Position of evil obstacle " << obs->x << ", " << obs->y  << std::endl;
+                std::cout << "Position of evil obstacle " << obs->x << ", " << obs->y << std::endl;
                 return false;
             }
         }
