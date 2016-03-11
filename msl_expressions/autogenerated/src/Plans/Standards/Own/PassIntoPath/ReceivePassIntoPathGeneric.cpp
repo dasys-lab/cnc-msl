46bf0886f03651f552d5f3b04803ccf5d5297430
Endy <awi@vs.uni-kassel.de>
2016-03-11 08:28:46 +0100

    pass into path preparation

---
+++ msl_expressions/autogenerated/src/Plans/Standards/Own/PassIntoPath/ReceivePassIntoPathGeneric.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+#include "Plans/Standards/Own/PassIntoPath/ReceivePassIntoPathGeneric.h"
+
+/*PROTECTED REGION ID(inccpp1457531583460) ENABLED START*/ //Add additional includes here
+/*PROTECTED REGION END*/
+namespace alica
+{
+    /*PROTECTED REGION ID(staticVars1457531583460) ENABLED START*/ //initialise static variables here
+    /*PROTECTED REGION END*/
+    ReceivePassIntoPathGeneric::ReceivePassIntoPathGeneric() :
+            DomainBehaviour("ReceivePassIntoPathGeneric")
+    {
+        /*PROTECTED REGION ID(con1457531583460) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+    ReceivePassIntoPathGeneric::~ReceivePassIntoPathGeneric()
+    {
+        /*PROTECTED REGION ID(dcon1457531583460) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+    void ReceivePassIntoPathGeneric::run(void* msg)
+    {
+        /*PROTECTED REGION ID(run1457531583460) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+    void ReceivePassIntoPathGeneric::initialiseParameters()
+    {
+        /*PROTECTED REGION ID(initialiseParameters1457531583460) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+/*PROTECTED REGION ID(methods1457531583460) ENABLED START*/ //Add additional methods here
+/*PROTECTED REGION END*/
+} /* namespace alica */
