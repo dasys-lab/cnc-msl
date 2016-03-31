96cc49342bbea4ab4809c81e0aae74db3e965a34
cn@donatello <cn@das-lab.net>
2016-03-31 19:58:26 +0200

    added Robotceck config to Robotcheck Behaviour

--- msl_expressions/autogenerated/include/Plans/Robotcheck/RobotTest.h
+++ msl_expressions/autogenerated/include/Plans/Robotcheck/RobotTest.h
@@ -8,45 +8,66 @@
 /*PROTECTED REGION END*/
 namespace alica
 {
-    class RobotTest : public DomainBehaviour
-    {
-    public:
-        RobotTest();
-        virtual ~RobotTest();
-        virtual void run(void* msg);
-        /*PROTECTED REGION ID(pub1456756113767) ENABLED START*/ //Add additional public methods here
-        /*PROTECTED REGION END*/
-    protected:
-        virtual void initialiseParameters();
-        /*PROTECTED REGION ID(pro1456756113767) ENABLED START*/ //Add additional protected methods here
-        int move;
+	class RobotTest : public DomainBehaviour
+	{
+	public:
+		RobotTest();
+		virtual ~RobotTest();
+		virtual void run(void* msg);
+		/*PROTECTED REGION ID(pub1456756113767) ENABLED START*/ //Add additional public methods here
+		/*PROTECTED REGION END*/
+	protected:
+		virtual void initialiseParameters();
+		/*PROTECTED REGION ID(pro1456756113767) ENABLED START*/ //Add additional protected methods here
+		int move;
 
-        bool driveForward;
-        bool driveBack;
-        bool rotateForward;
-        bool rotateBack;
-        bool kicker;
-        bool actuatorForward;
-        bool actuatorBack;
-        bool lightBarrier;
-        bool opticalFlow;
-        bool imu;
-        bool shovelSelectLow;
-        bool shovelSelectHigh;
-        double kickPower;
-        /*PROTECTED REGION END*/
-    private:
-        /*PROTECTED REGION ID(prv1456756113767) ENABLED START*/ //Add additional private methods here
-        bool translationRotationRobot(int movement, bool trans, int duration);
-        bool kickerRobot(int power);
-        bool actuatorRobot(int duration, int power);
-        bool lightBarrierRobot();
-        bool opticalFlowRobot();
-        bool imuRobot();
-        bool shovelSelectRobot(bool pass, int duration);
-        void printGlasses();
+		bool driveForward;
+		bool driveBack;
+		bool rotateForward;
+		bool rotateBack;
+		bool kicker;
+		bool actuatorForward;
+		bool actuatorBack;
+		bool lightBarrier;
+		bool opticalFlow;
+		bool imu;
+		bool shovelSelectLow;
+		bool shovelSelectHigh;
+		double kickPower;
+		bool startAll;
 
-        /*PROTECTED REGION END*/};
+		bool finDriveForward;
+		bool finDriveBack;
+		bool finRotateForward;
+		bool finRotateBack;
+		bool finKicker;
+		bool finActuatorForward;
+		bool finActuatorBack;
+		bool finLightBarrier;
+		bool finOpticalFlow;
+		bool finImu;
+		bool finShovelSelectLow;
+		bool finShovelSelectHigh;
+
+		bool out;
+		/*PROTECTED REGION END*/
+	private:
+		/*PROTECTED REGION ID(prv1456756113767) ENABLED START*/ //Add additional private methods here
+		bool translationRotationRobot(int movement, bool trans, int duration);
+		bool kickerRobot(int power);
+		bool actuatorRobot(int duration, int power);
+		bool lightBarrierRobot();
+		bool opticalFlowRobot();
+		bool imuRobot();
+		bool shovelSelectRobot(bool pass, int duration);
+		void printGlasses();
+//		bool readConfig(string pram);
+		void readConfigParms();
+		void setParms(bool b);
+		void setFinParmsFalse();
+		bool outPut(string s, bool t);
+		bool finished();
+		/*PROTECTED REGION END*/};
 } /* namespace alica */
 
 #endif /* RobotTest_H_ */
