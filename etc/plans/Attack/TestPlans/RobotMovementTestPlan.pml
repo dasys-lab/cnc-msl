be4d5f852de5035911e4f33723b12f2322110943
cn Basemant <cn@das-lab.net>
2016-05-23 13:33:21 +0200

    bla

--- etc/plans/Attack/TestPlans/RobotMovementTestPlan.pml
+++ etc/plans/Attack/TestPlans/RobotMovementTestPlan.pml
@@ -3,19 +3,12 @@
   <states id="1462969665132" name="Stop" comment="" entryPoint="1462969665133">
     <plans xsi:type="alica:BehaviourConfiguration">../../GenericBehaviours/Stop.beh#1413992626194</plans>
     <inTransitions>#1462969786619</inTransitions>
-    <inTransitions>#1462969919287</inTransitions>
     <outTransitions>#1462969759156</outTransitions>
   </states>
-  <states id="1462969698097" name="DribbleTest" comment="">
+  <states id="1462969884284" name="dribbleTest" comment="">
     <plans xsi:type="alica:BehaviourConfiguration">../../Behaviours/RobotMovementDribbleTest.beh#1462969753310</plans>
-    <inTransitions>#1462969917381</inTransitions>
-    <outTransitions>#1462969919287</outTransitions>
-  </states>
-  <states id="1462969884284" name="moveToBall" comment="">
-    <plans xsi:type="alica:BehaviourConfiguration">../../Behaviours/GetBall.beh#1414840399972</plans>
     <inTransitions>#1462969759156</inTransitions>
     <outTransitions>#1462969786619</outTransitions>
-    <outTransitions>#1462969917381</outTransitions>
   </states>
   <transitions id="1462969759156" name="MISSING_NAME" comment="start" msg="">
     <preCondition id="1462969760665" name="MISSING_NAME" comment="" conditionString="" pluginName="DefaultPlugin" enabled="true"/>
@@ -25,16 +18,6 @@
   <transitions id="1462969786619" name="MISSING_NAME" comment="stop" msg="">
     <preCondition id="1462969788458" name="MISSING_NAME" comment="" conditionString="" pluginName="DefaultPlugin" enabled="true"/>
     <inState>#1462969884284</inState>
-    <outState>#1462969665132</outState>
-  </transitions>
-  <transitions id="1462969917381" name="MISSING_NAME" comment="have ball" msg="">
-    <preCondition id="1462969919094" name="MISSING_NAME" comment="" conditionString="" pluginName="DefaultPlugin" enabled="true"/>
-    <inState>#1462969884284</inState>
-    <outState>#1462969698097</outState>
-  </transitions>
-  <transitions id="1462969919287" name="MISSING_NAME" comment="Stop" msg="">
-    <preCondition id="1462969921729" name="MISSING_NAME" comment="" conditionString="" pluginName="DefaultPlugin" enabled="true"/>
-    <inState>#1462969698097</inState>
     <outState>#1462969665132</outState>
   </transitions>
   <entryPoints id="1462969665133" name="MISSING_NAME" comment="" successRequired="false" minCardinality="0" maxCardinality="2147483647">
--- msl_expressions/autogenerated/src/Plans/Behaviours/RobotMovementDribbleTest.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/RobotMovementDribbleTest.cpp
@@ -7,132 +7,132 @@ using namespace std;
 /*PROTECTED REGION END*/
 namespace alica
 {
-	/*PROTECTED REGION ID(staticVars1462969724089) ENABLED START*/ //initialise static variables here
-	/*PROTECTED REGION END*/
-	RobotMovementDribbleTest::RobotMovementDribbleTest() :
-			DomainBehaviour("RobotMovementDribbleTest")
-	{
-		/*PROTECTED REGION ID(con1462969724089) ENABLED START*/ //Add additional options here
-		currentTarget = make_shared<geometry::CNPoint2D>();
-		attackPosY.push_back(wm->field->getFieldWidth() / 3.0 - 700);
-		attackPosY.push_back(0);
-		attackPosY.push_back(-wm->field->getFieldWidth() / 3.0 + 700);
-		/*PROTECTED REGION END*/
-	}
-	RobotMovementDribbleTest::~RobotMovementDribbleTest()
-	{
-		/*PROTECTED REGION ID(dcon1462969724089) ENABLED START*/ //Add additional options here
-		/*PROTECTED REGION END*/
-	}
-	void RobotMovementDribbleTest::run(void* msg)
-	{
-		/*PROTECTED REGION ID(run1462969724089) ENABLED START*/ //Add additional options here
-		msl::RobotMovement rm;
+    /*PROTECTED REGION ID(staticVars1462969724089) ENABLED START*/ //initialise static variables here
+    /*PROTECTED REGION END*/
+    RobotMovementDribbleTest::RobotMovementDribbleTest() :
+            DomainBehaviour("RobotMovementDribbleTest")
+    {
+        /*PROTECTED REGION ID(con1462969724089) ENABLED START*/ //Add additional options here
+        currentTarget = make_shared<geometry::CNPoint2D>();
+        attackPosY.push_back(wm->field->getFieldWidth() / 3.0 - 700);
+        attackPosY.push_back(0);
+        attackPosY.push_back(-wm->field->getFieldWidth() / 3.0 + 700);
+        /*PROTECTED REGION END*/
+    }
+    RobotMovementDribbleTest::~RobotMovementDribbleTest()
+    {
+        /*PROTECTED REGION ID(dcon1462969724089) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+    void RobotMovementDribbleTest::run(void* msg)
+    {
+        /*PROTECTED REGION ID(run1462969724089) ENABLED START*/ //Add additional options here
+        msl::RobotMovement rm;
 
-		auto ownPos = wm->rawSensorData->getOwnPositionVision();
-		auto ballPos = wm->ball->getEgoBallPosition();
-		auto dstscan = wm->rawSensorData->getDistanceScan();
+        auto ownPos = wm->rawSensorData->getOwnPositionVision();
+        auto ballPos = wm->ball->getEgoBallPosition();
+        auto dstscan = wm->rawSensorData->getDistanceScan();
 
-		// move to ball
-		msl_actuator_msgs::MotionControl bm;
-		query = make_shared<msl::MovementQuery>();
-		query->egoDestinationPoint = ballPos;
-		query->dribble = false;
+        // move to ball
+        msl_actuator_msgs::MotionControl bm;
+        query = make_shared<msl::MovementQuery>();
+        query->egoDestinationPoint = ballPos;
+        query->dribble = false;
 
-		if (wm->ball->haveBall())
-		{
-			query->dribble = true;
-			query->egoDestinationPoint = make_shared<geometry::CNPoint2D>(1,1)->alloToEgo(*ownPos);
-		}
-		bm = rm.experimentallyMoveToPoint(query);
+        if (wm->ball->haveBall())
+        {
+            query->dribble = true;
+            query->egoDestinationPoint = make_shared < geometry::CNPoint2D > (1, 1)->alloToEgo(*ownPos);
+        }
+        bm = rm.experimentallyMoveToPoint(query);
 
-		// dribble
-		/*
-		 if (ownPos == nullptr)
-		 return;
-		 if (currentTarget == nullptr)
-		 trueInitialize();
-		 if (currentTarget == nullptr)
-		 return;
-		 auto egoTarget = currentTarget->alloToEgo(*ownPos);
-		 if (egoTarget->length() < 1200)
-		 {
-		 this->setSuccess(true);
-		 }
+        // dribble
+        /*
+         if (ownPos == nullptr)
+         return;
+         if (currentTarget == nullptr)
+         trueInitialize();
+         if (currentTarget == nullptr)
+         return;
+         auto egoTarget = currentTarget->alloToEgo(*ownPos);
+         if (egoTarget->length() < 1200)
+         {
+         this->setSuccess(true);
+         }
 
-		 msl_actuator_msgs::MotionControl bm;
-		 shared_ptr < geometry::CNPoint2D > pathPlanningPoint;
-		 //bm = DribbleHelper.DribbleToPoint(egoTarget,this.dribbleVel,WM,out pathPlanningPoint);
-		 //		auto tmpMC = msl::RobotMovement::dribbleToPointConservative(egoTarget, pathPlanningPoint);
-		 query->egoDestinationPoint = egoTarget;
-		 query->dribble = true;
-		 auto tmpMC = rm.experimentallyMoveToPoint(query);
+         msl_actuator_msgs::MotionControl bm;
+         shared_ptr < geometry::CNPoint2D > pathPlanningPoint;
+         //bm = DribbleHelper.DribbleToPoint(egoTarget,this.dribbleVel,WM,out pathPlanningPoint);
+         //		auto tmpMC = msl::RobotMovement::dribbleToPointConservative(egoTarget, pathPlanningPoint);
+         query->egoDestinationPoint = egoTarget;
+         query->dribble = true;
+         auto tmpMC = rm.experimentallyMoveToPoint(query);
 
-		 //        Point2D oppInFront = ObstacleHelper.ClosestOpponentInCorridor(WM,ballPos.Angle(),300);
-		 //         double distInFront = (oppInFront==null?Double.MaxValue:oppInFront.Distance()-300);
-		 //
-		 //         double minInFrontDist = 1800;
-		 //         if (od!=null && od.Motion!=null) {
-		 //         minInFrontDist = Math.Max(minInFrontDist,Math.Min(2800,od.Motion.Translation+800));
-		 //         }
-		 //         if (ballPos != null && pathPlanningPoint!=null && Math.Abs(HHelper.DeltaAngle(pathPlanningPoint.Angle(),ballPos.Angle())) > Math.PI *4.75/6.0) {
-		 //         HHelper.SetTargetPoint(WorldHelper.Ego2Allo(pathPlanningPoint,ownPos));
-		 //         this.FailureStatus = true;
-		 //         } else if (ballPos!=null && dstscan!=null && distInFront < minInFrontDist && distInFront > 800){
-		 //         if(oppInFront!=null) HHelper.SetTargetPoint(WorldHelper.Ego2Allo(oppInFront.Rotate(Math.PI),ownPos));
-		 //         this.FailureStatus = true;
-		 //         }
-		 //		shared_ptr<geometry::CNPoint2D> turnTo = msl::RobotMovement::dribbleNeedToTurn(ownPos, ballPos,
-		 //																						pathPlanningPoint);
-		 //		if (turnTo!=nullptr) {
-		 //			HHelper.SetTargetPoint(turnTo);
-		 //			this->failure = true;
-		 //		}
-		 //if i drive in to the enemy goal area
-		 //		bm = msl::RobotMovement::nearGoalArea(bm);
-		 //        bm = DriveHelper.NearGoalArea(WM,bm);
-		 bm = rm.experimentallyRuleActionForBallGetter();
+         //        Point2D oppInFront = ObstacleHelper.ClosestOpponentInCorridor(WM,ballPos.Angle(),300);
+         //         double distInFront = (oppInFront==null?Double.MaxValue:oppInFront.Distance()-300);
+         //
+         //         double minInFrontDist = 1800;
+         //         if (od!=null && od.Motion!=null) {
+         //         minInFrontDist = Math.Max(minInFrontDist,Math.Min(2800,od.Motion.Translation+800));
+         //         }
+         //         if (ballPos != null && pathPlanningPoint!=null && Math.Abs(HHelper.DeltaAngle(pathPlanningPoint.Angle(),ballPos.Angle())) > Math.PI *4.75/6.0) {
+         //         HHelper.SetTargetPoint(WorldHelper.Ego2Allo(pathPlanningPoint,ownPos));
+         //         this.FailureStatus = true;
+         //         } else if (ballPos!=null && dstscan!=null && distInFront < minInFrontDist && distInFront > 800){
+         //         if(oppInFront!=null) HHelper.SetTargetPoint(WorldHelper.Ego2Allo(oppInFront.Rotate(Math.PI),ownPos));
+         //         this.FailureStatus = true;
+         //         }
+         //		shared_ptr<geometry::CNPoint2D> turnTo = msl::RobotMovement::dribbleNeedToTurn(ownPos, ballPos,
+         //																						pathPlanningPoint);
+         //		if (turnTo!=nullptr) {
+         //			HHelper.SetTargetPoint(turnTo);
+         //			this->failure = true;
+         //		}
+         //if i drive in to the enemy goal area
+         //		bm = msl::RobotMovement::nearGoalArea(bm);
+         //        bm = DriveHelper.NearGoalArea(WM,bm);
+         bm = rm.experimentallyRuleActionForBallGetter();
 
-		 bm = tmpMC;*/
-		send(bm);
-		/*PROTECTED REGION END*/
-	}
-	void RobotMovementDribbleTest::initialiseParameters()
-	{
-		/*PROTECTED REGION ID(initialiseParameters1462969724089) ENABLED START*/ //Add additional options here
-		currentTarget = nullptr;
-		msl::RobotMovement::reset();
-		trueInitialize();
-		/*PROTECTED REGION END*/
-	}
-	/*PROTECTED REGION ID(methods1462969724089) ENABLED START*/ //Add additional methods here
-	void RobotMovementDribbleTest::trueInitialize() // so true and so evil
-	{
-		auto ownPos = wm->rawSensorData->getOwnPositionVision();
-		if (ownPos == nullptr)
-			return;
-		//		Random rand = new Random();
-		//		int index = (int)Math.Round(rand.NextDouble()*2.0);
+         bm = tmpMC;*/
+        send(bm);
+        /*PROTECTED REGION END*/
+    }
+    void RobotMovementDribbleTest::initialiseParameters()
+    {
+        /*PROTECTED REGION ID(initialiseParameters1462969724089) ENABLED START*/ //Add additional options here
+        currentTarget = nullptr;
+        msl::RobotMovement::reset();
+        trueInitialize();
+        /*PROTECTED REGION END*/
+    }
+    /*PROTECTED REGION ID(methods1462969724089) ENABLED START*/ //Add additional methods here
+    void RobotMovementDribbleTest::trueInitialize() // so true and so evil
+    {
+        auto ownPos = wm->rawSensorData->getOwnPositionVision();
+        if (ownPos == nullptr)
+            return;
+        //		Random rand = new Random();
+        //		int index = (int)Math.Round(rand.NextDouble()*2.0);
 
-		srand(supplementary::DateTime::getUtcNowC());
-		int index = (rand() % attackPosY.size());
+        srand(supplementary::DateTime::getUtcNowC());
+        int index = (rand() % attackPosY.size());
 
-		if (ownPos->x < wm->field->getFieldLength() / 6.0)
-		{
-			currentTarget = make_shared<geometry::CNPoint2D>(wm->field->getFieldLength() / 6.0 - 1500, 0);
-			//} else if (ownPos.X < field.FieldLength/2.0) {
-			//	currentTarget = new Point2D(field.FieldLength/2.0,0);
-		}
-		else
-		{
-			currentTarget = make_shared<geometry::CNPoint2D>(wm->field->getFieldLength() / 4.0 - 1500, 0);
-		}
-		currentTarget->y = attackPosY.at(index);
-		if (currentTarget->alloToEgo(*ownPos)->length() < 1500)
-		{
-			index = (index + 1) % attackPosY.size(); // select next point in vector
-			currentTarget->y = attackPosY.at(index);
-		}
-	}
+        if (ownPos->x < wm->field->getFieldLength() / 6.0)
+        {
+            currentTarget = make_shared < geometry::CNPoint2D > (wm->field->getFieldLength() / 6.0 - 1500, 0);
+            //} else if (ownPos.X < field.FieldLength/2.0) {
+            //	currentTarget = new Point2D(field.FieldLength/2.0,0);
+        }
+        else
+        {
+            currentTarget = make_shared < geometry::CNPoint2D > (wm->field->getFieldLength() / 4.0 - 1500, 0);
+        }
+        currentTarget->y = attackPosY.at(index);
+        if (currentTarget->alloToEgo(*ownPos)->length() < 1500)
+        {
+            index = (index + 1) % attackPosY.size(); // select next point in vector
+            currentTarget->y = attackPosY.at(index);
+        }
+    }
 /*PROTECTED REGION END*/
 } /* namespace alica */
