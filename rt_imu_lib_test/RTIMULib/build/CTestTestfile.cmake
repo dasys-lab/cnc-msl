ad897cbba5279045fcafbbd9adeadc22cdfda020
Stephan Opfer <opfer@vs.uni-kassel.de>
2016-06-10 14:16:44 +0200

    remove build files

--- rt_imu_lib_test/RTIMULib/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emmeda/Research/dev/mslws/src/cnc-msldriver/rt_imu_lib_test/RTIMULib
-# Build directory: /home/emmeda/Research/dev/mslws/src/cnc-msldriver/rt_imu_lib_test/RTIMULib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
