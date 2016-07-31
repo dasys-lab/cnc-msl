ea537094c031bdd12fb9aa7a85eb08ee83f35c3f
Lisa Martmann <l.martmann@gmx.de>
2016-07-31 15:28:27 +0200

    moved consts into protected region, they were overwritten upon code generation and caused errors

--- msl_expressions/autogenerated/include/Plans/DribbleCalibration/Behaviours/CalibrationTakeBall.h
+++ msl_expressions/autogenerated/include/Plans/DribbleCalibration/Behaviours/CalibrationTakeBall.h
@@ -7,52 +7,51 @@
 /*PROTECTED REGION END*/
 namespace alica
 {
-	class CalibrationTakeBall : public DomainBehaviour
-	{
-	public:
-		CalibrationTakeBall();
-		virtual ~CalibrationTakeBall();
-		virtual void run(void* msg);
-		/*PROTECTED REGION ID(pub1469109429392) ENABLED START*/ //Add additional public methods here
-		/*PROTECTED REGION END*/
-	protected:
+    class CalibrationTakeBall : public DomainBehaviour
+    {
+    public:
+        CalibrationTakeBall();
+        virtual ~CalibrationTakeBall();
+        virtual void run(void* msg);
+        /*PROTECTED REGION ID(pub1469109429392) ENABLED START*/ //Add additional public methods here
+        /*PROTECTED REGION END*/
+    protected:
+        virtual void initialiseParameters();
+        /*PROTECTED REGION ID(pro1469109429392) ENABLED START*/ //Add additional protected methods here
+        // consts for checkBallRotation()
+        static const int ROTATE_CORRECT = 0;
+        static const int ROTATE_LEFT = 10;
+        static const int ROTATE_RIGHT = 20;
+        //static const int ROTATE_BACKWARDS = 30;
+        static const int ROTATE_TOO_SLOW = 40;
+        static const int ROTATE_ERR = -10;
+        DribbleCalibrationContainer dcc;
 
-		// consts for checkBallRotation()
-		static const int ROTATE_CORRECT = 0;
-		static const int ROTATE_LEFT = 10;
-		static const int ROTATE_RIGHT = 20;
-		//static const int ROTATE_BACKWARDS = 30;
-		static const int ROTATE_TOO_SLOW = 40;
-		static const int ROTATE_ERR = -10;
+        bool ballRotateCorrect;
+        bool ballHoldCorrect;
 
-		virtual void initialiseParameters();
-		/*PROTECTED REGION ID(pro1469109429392) ENABLED START*/ //Add additional protected methods here
-		DribbleCalibrationContainer dcc;
+        // config parameters
+        double speedNoBall;
+        double slowTranslationWheelSpeed;
+        double minRotation;
+        // left and right are swapped
+        double dribbleFactorLeft;
+        double dribbleFactorRight;
 
-		bool ballRotateCorrect;bool ballHoldCorrect;
+        // for correctRightWheelSpeed function
+        double dribbleFactorRightOld;
+        double changingFactor;
+        int defectWheel;
 
-		// config parameters
-		double speedNoBall;
-		double slowTranslationWheelSpeed;
-		double minRotation;
-		// left and right are swapped
-		double dribbleFactorLeft;
-		double dribbleFactorRight;
+        int checkBallRotation();
+        void correctWheelSpeed(int wheel);
+        void readConfigParameters();
+        void writeConfigParameters();
 
-		// for correctRightWheelSpeed function
-		double dribbleFactorRightOld;
-		double changingFactor;
-		int defectWheel;
-
-		int checkBallRotation();
-		void correctWheelSpeed(int wheel);
-		void readConfigParameters();
-		void writeConfigParameters();
-
-		/*PROTECTED REGION END*/
-	private:
-		/*PROTECTED REGION ID(prv1469109429392) ENABLED START*/ //Add additional private methods here
-		/*PROTECTED REGION END*/};
+        /*PROTECTED REGION END*/
+    private:
+        /*PROTECTED REGION ID(prv1469109429392) ENABLED START*/ //Add additional private methods here
+        /*PROTECTED REGION END*/};
 } /* namespace alica */
 
 #endif /* CalibrationTakeBall_H_ */
