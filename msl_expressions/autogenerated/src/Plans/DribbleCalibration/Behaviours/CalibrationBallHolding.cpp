f8f44f736fa5e9e15292ca2859d5fa2bbd9c3a2e
Carpe Noctem <cn@das-lab.net>
2016-08-31 15:00:50 +0200

    adatp stuff again

--- msl_expressions/autogenerated/src/Plans/DribbleCalibration/Behaviours/CalibrationBallHolding.cpp
+++ msl_expressions/autogenerated/src/Plans/DribbleCalibration/Behaviours/CalibrationBallHolding.cpp
@@ -25,6 +25,7 @@ namespace alica
 	void CalibrationBallHolding::run(void* msg)
 	{
 		/*PROTECTED REGION ID(run1469284294147) ENABLED START*/ //Add additional options here
+		this->setSuccess(true);
 		return;
 		if (wm->rawSensorData->getLightBarrier())
 		{
