6fa8704238b8b69daf09803bf0ec5491677a4f56
Carpe Noctem <cn@das-lab.net>
2016-09-14 15:48:18 +0200

    removed old commentary

--- msl_robot/include/msl_robot/robotmovement/RobotMovement.h
+++ msl_robot/include/msl_robot/robotmovement/RobotMovement.h
@@ -39,99 +39,19 @@ namespace msl
 		msl_actuator_msgs::MotionControl driveRandomly(double translation);
 		msl_actuator_msgs::MotionControl moveToFreeSpace(shared_ptr<MovementQuery> m_Query);
 
-//		static msl_actuator_msgs::MotionControl moveToPointFast(shared_ptr<geometry::CNPoint2D> egoTarget,
-//																shared_ptr<geometry::CNPoint2D> egoAlignPoint,
-//																double snapDistance,
-//																shared_ptr<vector<shared_ptr<geometry::CNPoint2D>>> additionalPoints);
-//		static msl_actuator_msgs::MotionControl moveToPointCarefully(shared_ptr<geometry::CNPoint2D>egoTarget,
-//													shared_ptr<geometry::CNPoint2D> egoAlignPoint, double snapDistance, shared_ptr<vector<shared_ptr<geometry::CNPoint2D>>> additionalPoints = nullptr);
-//		static MotionControl interceptCarefully(shared_ptr<geometry::CNPoint2D> egoTarget,
-//												shared_ptr<geometry::CNPoint2D> egoAlignPoint, double snapDistance, shared_ptr<vector<shared_ptr<geometry::CNPoint2D>>> additionalPoints = nullptr);
-		//TODO needs to be tested
-//		static msl_actuator_msgs::MotionControl alignToPointNoBall(shared_ptr<geometry::CNPoint2D> egoTarget,
-//												shared_ptr<geometry::CNPoint2D> egoAlignPoint, double angleTolerance);
-//		static msl_actuator_msgs::MotionControl alignToPointWithBall(shared_ptr<geometry::CNPoint2D> egoAlignPoint,
-//													shared_ptr<geometry::CNPoint2D> egoBallPos, double angleTolerance,
-//													double ballAngleTolerance);
-		//TODO needs to be tested
-//		static msl_actuator_msgs::MotionControl rapidAlignToPointWithBall(shared_ptr<geometry::CNPoint2D> egoAlignPoint,
-//													shared_ptr<geometry::CNPoint2D> egoBallPos, double angleTolerance,
-//													double ballAngleTolerance);
-
-		//TODO needs to be tested
-//		static msl_actuator_msgs::MotionControl ruleActionForBallGetter();
-
-		//TODO needs to be implemented
-//		static msl_actuator_msgs::MotionControl driveRandomly(double translation);
-
-		//TODO needs to be tested
-//		static msl_actuator_msgs::MotionControl placeRobotCareBall(shared_ptr<geometry::CNPoint2D> destinationPoint, shared_ptr<geometry::CNPoint2D> headingPoint, double translation);
-
-		//TODO needs to be tested
-//		static msl_actuator_msgs::MotionControl placeRobot(shared_ptr<geometry::CNPoint2D> destinationPoint, shared_ptr<geometry::CNPoint2D> headingPoint, double translation);
-
-		//TODO needs to be tested
-//		static msl_actuator_msgs::MotionControl placeRobotAggressive(shared_ptr<geometry::CNPoint2D> destinationPoint, shared_ptr<geometry::CNPoint2D> headingPoint, double translation);
-
-		//TODO needs to be tested
-//		static msl_actuator_msgs::MotionControl moveToFreeSpace(shared_ptr<geometry::CNPoint2D> alloPassee, double maxTrans);
-
-
-//		static msl_actuator_msgs::MotionControl driveToPointAlignNoAvoidance(shared_ptr<geometry::CNPoint2D> destination, shared_ptr<geometry::CNPoint2D> alignPoint,
-//	                                                         double translation, bool alignSlow);
-//		static msl_actuator_msgs::MotionControl driveToPointNoAvoidance(shared_ptr<geometry::CNPoint2D> egoDest, double translation);
-//		static msl_actuator_msgs::MotionControl align(MotionControl bm, shared_ptr<geometry::CNPoint2D> alignPoint, double rotTol, bool slow);
-
-//		static msl_actuator_msgs::MotionControl driveRandomly(int translation);
-//		static shared_ptr<msl_actuator_msgs::MotionControl> dribbleToPointConservative(shared_ptr<geometry::CNPoint2D> goalMid, shared_ptr<geometry::CNPoint2D>& ppp);
-//		static shared_ptr<geometry::CNPoint2D> dribbleNeedToTurn(shared_ptr<geometry::CNPosition> own, shared_ptr<geometry::CNPoint2D> ballPos, shared_ptr<geometry::CNPoint2D> pathPlanningPoint);
-//		static msl_actuator_msgs::MotionControl nearGoalArea(msl_actuator_msgs::MotionControl bm);
-//		static void reset();
-//		static void updateLastTurnTime();
-
-//		static shared_ptr<geometry::CNPoint2D> getRandomTarget();
 		static void readConfigParameters();
 		static double defaultTranslation;
 		static double defaultRotateP;
 		static double fastTranslation;
 		static double fastRotation;
-//		static double interceptCarfullyRotateP;
-//		static double maxVel;
 
 	private:
-//		static double lastRotError;
-//		static double lastRotErrorWithBall;
-//		static double alignToPointMinRotation;
-//		static double alignToPointMaxRotation;
-//		static double lastRotErrorWithBallRapid;
-//		static double alignToPointRapidMaxRotation;
-//		static double alignToPointpRot;
-//		static double alignMaxVel;
-//		static double maxVelo;
 		static int randomCounter;
 		static int beamSize;
 		static shared_ptr<vector<shared_ptr<SearchArea>>> fringe;
 		static shared_ptr<vector<shared_ptr<SearchArea>>> next;
 		static shared_ptr<geometry::CNPoint2D> randomTarget;
 
-//		static double lastRotErr;
-//		static double curRot;
-//		static double curTrans;
-//		static double pRot;
-//		static double dRot;
-//		static double rotAccStep;
-//		static double maxRot;
-//		static double angleDeadBand;
-//		static double transControlIntegral;
-//		static double transControlIntegralMax;
-//		static double iTrans;
-//		static double pTrans;
-//		static double transAccStep;
-//		static double transDecStep;
-
-//		static double lastTurnTime;
-
-		//shared_ptr<MovementQuery> query;
 		MSLWorldModel* wm;
 		PathProxy* pp;
 		msl_actuator_msgs::MotionControl placeRobot(shared_ptr<geometry::CNPoint2D> dest, shared_ptr<geometry::CNPoint2D> headingPoint);
@@ -140,8 +60,6 @@ namespace msl
 		msl_actuator_msgs::MotionControl setNAN();
 
 	protected:
-//		static double evalPointDynamic(shared_ptr<geometry::CNPoint2D> alloP, shared_ptr<geometry::CNPoint2D> alloPassee,
-//									   shared_ptr<geometry::CNPosition> ownPos, shared_ptr<vector<shared_ptr<geometry::CNPoint2D>>> opponents);
 		static double assume_enemy_velo;
 		static double assume_ball_velo;
 		static double interceptQuotient;
