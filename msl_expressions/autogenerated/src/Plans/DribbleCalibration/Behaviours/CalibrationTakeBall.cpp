78c90e37af603527888c1c86ac6ce8842915e19e
Carpe Noctem <cn@das-lab.net>
2016-09-29 16:56:15 +0300

    changed stuff on nase

--- msl_expressions/autogenerated/src/Plans/DribbleCalibration/Behaviours/CalibrationTakeBall.cpp
+++ msl_expressions/autogenerated/src/Plans/DribbleCalibration/Behaviours/CalibrationTakeBall.cpp
@@ -30,8 +30,8 @@ namespace alica
     {
         /*PROTECTED REGION ID(run1469109429392) ENABLED START*/ //Add additional options here
         // TODO: remove when finished testing!
-//        this->setSuccess(true);
-//        return;
+        this->setSuccess(true);
+        return;
         // check if robot has the ball
         if (wm->rawSensorData->getLightBarrier())
         {
