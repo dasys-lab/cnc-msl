0351e716b248b51de8a0d2941b6cd126d571a3e3
Lisa Martmann <l.martmann@gmx.de>
2017-01-17 19:46:03 +0100

    made all remaining constraints crash-safe and started conforming standards to rules 2017

--- msl_expressions/autogenerated/src/Plans/Standards/Own/ThrowIn/ReceiveInOppHalf.cpp
+++ msl_expressions/autogenerated/src/Plans/Standards/Own/ThrowIn/ReceiveInOppHalf.cpp
@@ -47,28 +47,35 @@ namespace alica
         if (alloBallPose->y < 0.0) // right side line
         {
             // place the receiver 1m outside the sideline
-            if (wm->field->getSurrounding() > 1300)
-            {
-                yCordOfReceiver = -wm->field->getFieldWidth() / 2.0 - 1000.0;
-            }
-            else
-            {
-                yCordOfReceiver = -wm->field->getFieldWidth() / 2.0 - wm->field->getSurrounding()
-                        + wm->pathPlanner->getRobotRadius() + securityReceiver;
-            }
+//            if (wm->field->getSurrounding() > 1300)
+//            {
+//                yCordOfReceiver = -wm->field->getFieldWidth() / 2.0 - 1000.0;
+//            }
+//            else
+//            {
+//                yCordOfReceiver = -wm->field->getFieldWidth() / 2.0 - wm->field->getSurrounding()
+//                        + wm->pathPlanner->getRobotRadius() + securityReceiver;
+//            }
+
+            // ^ forbidden with rule changes 2017. place robot on side line instead
+            yCordOfReceiver = -wm->field->getFieldWidth() / 2.0 + securityReceiver;
         }
         else // left side line
         {
             // place the receiver 1m outside the sideline
-            if (wm->field->getSurrounding() > 1300)
-            {
-                yCordOfReceiver = wm->field->getFieldWidth() / 2.0 + 1000.0;
-            }
-            else
-            {
-                yCordOfReceiver = wm->field->getFieldWidth() / 2.0 + wm->field->getSurrounding()
-                        - wm->pathPlanner->getRobotRadius() - securityReceiver;
-            }
+//			if (wm->field->getSurrounding() > 1300)
+//			{
+//				yCordOfReceiver = wm->field->getFieldWidth() / 2.0 + 1000.0;
+//			}
+//			else
+//			{
+//				yCordOfReceiver = wm->field->getFieldWidth() / 2.0 + wm->field->getSurrounding()
+//						- wm->pathPlanner->getRobotRadius() - securityReceiver;
+//			}
+
+            // ^ forbidden with rule changes 2017. place robot on side line instead
+
+            yCordOfReceiver = wm->field->getFieldWidth() / 2.0 - securityReceiver;
         }
 
         double lowestX = wm->field->getFieldLength() / 2;
