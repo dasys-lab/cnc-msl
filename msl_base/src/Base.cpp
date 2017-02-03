38347c1da823b18d0219508f8bdcb58c42b34463
Stephan Opfer <opfer@vs.uni-kassel.de>
2017-02-03 18:04:52 +0100

    - fixed extra indentation for curly braces - formatted base correspondingly

--- msl_base/src/Base.cpp
+++ msl_base/src/Base.cpp
@@ -34,19 +34,19 @@ Base::Base(string roleSetName, string masterPlanName, string roleSetDir, bool si
     ae->setIAlicaClock(new alicaRosProxy::AlicaROSClock());
     ae->setCommunicator(new alicaRosProxy::AlicaRosCommunication(ae));
     if (sim)
-        {
-            cout << "Base Vorher: " << ae->getIAlicaClock()->now() << endl;
-            ae->getIAlicaClock()->sleep(200000);
-            cout << "Base Nachher: " << ae->getIAlicaClock()->now() << endl;
-        }
+    {
+        cout << "Base Vorher: " << ae->getIAlicaClock()->now() << endl;
+        ae->getIAlicaClock()->sleep(200000);
+        cout << "Base Nachher: " << ae->getIAlicaClock()->now() << endl;
+    }
 
     ae->addSolver(SolverType::GRADIENTSOLVER, new alica::reasoner::CGSolver(ae));
 
     wm = MSLWorldModel::get();
     if (sim)
-        {
-            wm->timeLastSimMsgReceived = 1;
-        }
+    {
+        wm->timeLastSimMsgReceived = 1;
+    }
     wm->setEngine(ae);
 
     ae->init(bc, cc, uc, crc, roleSetName, masterPlanName, roleSetDir, false);
@@ -87,10 +87,10 @@ string getNodeName(string postFix)
 int main(int argc, char **argv)
 {
     if (argc < 2)
-        {
-            printUsage();
-            return 0;
-        }
+    {
+        printUsage();
+        return 0;
+    }
     cout << "Initialising ROS" << endl;
     ros::init(argc, argv, getNodeName("Base"));
     // This makes segfaults to exceptions
@@ -101,33 +101,33 @@ int main(int argc, char **argv)
     string roleset = "";
     bool sim = false;
     for (int i = 1; i < argc; i++)
+    {
+        if (string(argv[i]) == "-m" || string(argv[i]) == "-masterplan")
         {
-            if (string(argv[i]) == "-m" || string(argv[i]) == "-masterplan")
-                {
-                    masterplan = argv[i + 1];
-                    i++;
-                }
+            masterplan = argv[i + 1];
+            i++;
+        }
 
-            if (string(argv[i]) == "-rd" || string(argv[i]) == "-rolesetdir")
-                {
-                    rolesetdir = argv[i + 1];
-                    i++;
-                }
-            if (string(argv[i]) == "-r" || string(argv[i]) == "-roleset")
-                {
-                    roleset = argv[i + 1];
-                    i++;
-                }
-            if (string(argv[i]) == "-sim")
-                {
-                    sim = true;
-                }
-        }
-    if (masterplan.size() == 0 || rolesetdir.size() == 0)
+        if (string(argv[i]) == "-rd" || string(argv[i]) == "-rolesetdir")
         {
-            printUsage();
-            return 0;
+            rolesetdir = argv[i + 1];
+            i++;
         }
+        if (string(argv[i]) == "-r" || string(argv[i]) == "-roleset")
+        {
+            roleset = argv[i + 1];
+            i++;
+        }
+        if (string(argv[i]) == "-sim")
+        {
+            sim = true;
+        }
+    }
+    if (masterplan.size() == 0 || rolesetdir.size() == 0)
+    {
+        printUsage();
+        return 0;
+    }
     cout << "\tMasterplan is:       \"" << masterplan << "\"" << endl;
     cout << "\tRolset Directory is: \"" << rolesetdir << "\"" << endl;
     cout << "\tRolset is:           \"" << (roleset.empty() ? "Default" : roleset) << "\"" << endl;
@@ -139,10 +139,10 @@ int main(int argc, char **argv)
     base->start();
 
     while (ros::ok())
-        {
-            std::chrono::milliseconds dura(500);
-            std::this_thread::sleep_for(dura);
-        }
+    {
+        std::chrono::milliseconds dura(500);
+        std::this_thread::sleep_for(dura);
+    }
     delete base;
 
     return 0;
