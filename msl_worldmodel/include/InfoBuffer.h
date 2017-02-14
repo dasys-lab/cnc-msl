7fc257c6d5761f465ad546832cafa11f5b39253e
Stephan Opfer <opfer@vs.uni-kassel.de>
2017-02-14 23:52:25 +0100

    hmmm

--- msl_worldmodel/include/InfoBuffer.h
+++ msl_worldmodel/include/InfoBuffer.h
@@ -1,144 +1,45 @@
 #pragma once
 
+#include "InformationElement.h"
+
 #include <memory>
 #include <mutex>
 #include <typeinfo>
+
 
 namespace msl
 {
 /**
  * Information buffer of shared_ptr of a given data type.
- *
  */
-template <typename T> class InfoBuffer
+template <typename T>
+class InfoBuffer
 {
   public:
-	/**
-	 * Default constructor.
-	 * @param bufferSize Number of elements which can be stored within the information buffer
-	 */
-    inline InfoBuffer(const int bufferSize)
-    {
-        this->bufferSize = bufferSize;
-        this->infoElementCounter = 0;
-        this->index = -1;
-        this->ringBuffer = std::unique_ptr<std::shared_ptr<T>[]>(new std::shared_ptr<T>[this->bufferSize]);
-    }
+    InfoBuffer(const int bufferSize);
+    virtual ~InfoBuffer();
 
-    /**
-     *  Default destructor.
-     */
-    inline virtual ~InfoBuffer()
-    {
-    }
+    const int getBufferSize() const;
+    const int getSize() const;
+    void clear(bool cleanBuffer);
+    const std::type_info *getTypeInfo() const;
 
-    /**
-     * Returns the last element or NULL if no element exists.
-     * @return
-     */
-    std::shared_ptr<T> getLast()
-    {
-        return this->getLast(0);
-    }
+    int add(InformationElement<T> element);
 
-    /**
-     * Returns the n-th last element. Returns NULL if no element exists or n > maxBuffer.
-     * @param n The n-th last element.
-     * @return The n-th last element, 0 will return the newest one.
-     */
-    std::shared_ptr<T> getLast(int n)
-    {
-        std::shared_ptr<T> ptr;
-        std::lock_guard<std::mutex> guard(mtx_);
+    InformationElement<T> getLast(const int n = 0) const;
+    InformationElement<T> getLastValid() const;
 
-        if (this->index < 0 || this->bufferSize <= n || this->infoElementCounter <= n)
-        {
-            return ptr;
-        }
-
-        int index = (this->index - n) % this->bufferSize;
-
-        ptr = this->ringBuffer[index];
-
-        return ptr;
-    }
-
-    /**
-     * Adds a new element to the information buffer and returns the identifier.
-     * @param element The element to add.
-     * @return the number of elements in the buffer.
-     */
-    int add(std::shared_ptr<T> element)
-    {
-        std::lock_guard<std::mutex> guard(mtx_);
-
-        int index = (++this->index) % this->bufferSize;
-
-        this->ringBuffer[index] = element;
-
-        return this->infoElementCounter++;
-    }
-
-    /**
-     * Return the buffer size.
-     * @return the buffer size.
-     */
-    const int getBufferSize() const
-    {
-        return this->bufferSize;
-    }
-
-
-    /**
-     * Returns the current count of elements within the information buffer.
-     * @return the current count of elements within the information buffer.
-     */
-    const int getSize() const
-    {
-        return (this->bufferSize < this->infoElementCounter) ? this->bufferSize : this->infoElementCounter;
-    }
-
-    /*!
-     * \brief Clears the buffer.
-     *
-     * Clears the buffer. If cleanBuffer is false only the index structure is reseted, but the
-     * buffer still exists (old elements are not accessible). If cleanBuffer is true the
-     * pointers from the buffer are cleared as well.
-     *
-     * \param cleanBuffer True to clear the buffer.
-     */
-    int clear(bool cleanBuffer)
-    {
-        std::lock_guard<std::mutex> guard(mtx_);
-        this->index = -1;
-        this->infoElementCounter = 0;
-
-        if (false == cleanBuffer)
-            return 0;
-
-        std::shared_ptr<T> ptr;
-        for (int i = 0; i < this->bufferSize; ++i)
-            this->ringBuffer[i] = ptr;
-
-        return 0;
-    }
-
-    /*!
-     *\brief Returns the type_info of the used template type.
-     *
-     * Returns the type_info of the used template type.
-     */
-    const std::type_info *getTypeInfo() const
-    {
-        return &typeid(T);
-    }
+    InformationElement<T> getTemporalClose(const InfoTime time) const;
+    InformationElement<T> getTemporalClose(const InfoTime time, const InfoTime timeInterval) const;
 
   private:
     std::mutex mtx_;
-    std::unique_ptr<std::shared_ptr<T>[]> ringBuffer; /**< Ring buffer of elements */
+    std::unique_ptr<InformationElement<T>[]> ringBuffer; /**< Ring buffer of elements */
     int bufferSize;                                   /**< number of stored elements */
-    ulong infoElementCounter;                         /**< Counter of elements added to the ring buffer */
-    long index;                                       /**< Current index of the last added element */
+    int index;                                        /**< Current index of the last added element */
+    unsigned long long infoElementCounter;            /**< Counter of elements added to the buffer */
 };
 
-} /* namespace ice */
+} /* namespace msl */
+
+
--- msl_worldmodel/include/InformationElement.h
+++ msl_worldmodel/include/InformationElement.h
@@ -9,24 +9,27 @@ typedef unsigned long long InfoTime;
 /**
  * Information element stores one information and provides required meta
  * data. The information should not be changed.
- *
  */
-template <typename T> class InformationElement
+template <typename T>
+class InformationElement
 {
   public:
     InformationElement(std::shared_ptr<T> information, InfoTime creationTime, InfoTime validityTime, double certainty);
+    InformationElement();
     virtual ~InformationElement();
 
     std::shared_ptr<T> getInformation() const;
     InfoTime getCreationTime() const;
     InfoTime getValidityTime() const;
     double getCertainty() const;
+    bool isValid() const;
 
   private:
     std::shared_ptr<T> information; /**< the stored information */
     InfoTime creationTime;          /**< reception time of the information */
     InfoTime validityTime;          /**< the latest time this information is considered to be valid */
     double certainty;               /**< how certain the information was at the moment it was created */
+    bool isDummy;
 };
 
 } /* namespace msl */
