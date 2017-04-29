29e6ca178ec2fa481d88bfcf113192707cfa3026
cn-pinky <cn@das-lab.net>
2017-04-29 12:35:43 +0200

    fix bug in DribbleControl

--- msl_expressions/autogenerated/src/Plans/TestPlans/DribbleControlTest/DribbleControlMOS.cpp
+++ msl_expressions/autogenerated/src/Plans/TestPlans/DribbleControlTest/DribbleControlMOS.cpp
@@ -160,7 +160,7 @@ namespace alica
 		else
 		{
 			// rotate
-			velX = velX - epsilonRot * sign(rotation) * rotation * rotation * rBallRobot;
+			velX = velX - epsilonRot * rotation * rotation * rBallRobot;
 		}
 		cout << "velY = " << velXTemp << "+ 3/4 * " << rBallRobot << " * " << rotation;
 		velY = velYTemp + 3.0 / 4.0 * rBallRobot * rotation;
