c768247bef241edca437878fa3b73d41bb31128c
cn-pinky <cn@das-lab.net>
2017-06-06 11:35:37 +0200

    wheel speed oops

--- msl_expressions/autogenerated/src/Plans/TwoHoledWall/AlignAndShootTwoHoledWall.cpp
+++ msl_expressions/autogenerated/src/Plans/TwoHoledWall/AlignAndShootTwoHoledWall.cpp
@@ -115,8 +115,10 @@ namespace alica
 
         // Constant ball handle wheel speed
         BallHandleCmd bhc;
-        bhc.leftMotor = (int8_t)this->wheelSpeed;
-        bhc.rightMotor = (int8_t)this->wheelSpeed;
+//        bhc.leftMotor = (int8_t)this->wheelSpeed;
+//        bhc.rightMotor = (int8_t)this->wheelSpeed;
+        bhc.leftMotor = this->wheelSpeed;
+        bhc.rightMotor = this->wheelSpeed;
         send(bhc);
 
         // Create ego-centric 2D target...
