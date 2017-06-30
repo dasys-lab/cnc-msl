016e6d551b1af1cd1ee197079ed332ad3c95750b
Marcel Hesse <marcel-hesse@hotmail.de>
2017-06-30 12:48:17 +0200

    Domain Specfic Changes to DomainBehaviour and DomainCondition

--- msl_expressions/autogenerated/include/DomainCondition.h
+++ msl_expressions/autogenerated/include/DomainCondition.h
@@ -2,37 +2,20 @@
 #define DomainBehaviour_H_
 
 #include "engine/BasicCondition.h"
+/*PROTECTED REGION ID(domainHeaderAdditional) ENABLED START*/
+// Add additional options here
+/*PROTECTED REGION END*/
 
-namespace msl{
-	class MSLWorldModel;
-	class MSLRobot;
-	class Rules;
-}
+namespace alica {
+class DomainCondition : public BasicCondition {
+public:
+  DomainCondition();
+  virtual ~DomainCondition();
 
-namespace supplementary {
-	class SystemConfig;
-}
-
-namespace alica
-{
-	class DomainCondition : public BasicCondition
-	{
-	public:
-		DomainCondition();
-		virtual ~DomainCondition();
-
-		msl::MSLWorldModel* wm;
-		msl::MSLRobot* robot;
-		supplementary::SystemConfig* sc;
-		msl::Rules* rules;
-
-		/* The time that can pass since the last start command
-		 * until we need to skip proper positioning and execute
-		 * the standard right away. [ns]
-		 */
-		uint64_t timeUntilEmergencyExecute;
-	};
+  /*PROTECTED REGION ID(domainHeader) ENABLED START*/
+  // Add additional options here
+  /*PROTECTED REGION END*/
+};
 } /* namespace alica */
 
 #endif /* DomainBehaviour_H_ */
-
--- msl_expressions/autogenerated/src/DomainCondition.cpp
+++ msl_expressions/autogenerated/src/DomainCondition.cpp
@@ -1,22 +1,18 @@
 #include "DomainCondition.h"
-#include <SystemConfig.h>
-#include <MSLWorldModel.h>
-#include <msl_robot/MSLRobot.h>
-#include <Rules.h>
+/*PROTECTED REGION ID(domainSourceHeaders) ENABLED START*/
+// Add additional options here
+/*PROTECTED REGION END*/
 
-namespace alica
-{
-	DomainCondition::DomainCondition() :
-			BasicCondition()
-	{
-		this->wm = msl::MSLWorldModel::get();
-		this->robot = msl::MSLRobot::get();
-		this->rules = msl::Rules::getInstance();
-		this->sc = supplementary::SystemConfig::getInstance();
-		this->timeUntilEmergencyExecute = this->rules->getStandbyTime() *0.8;
-	}
+namespace alica {
+DomainCondition::DomainCondition() : BasicCondition() {
+  /*PROTECTED REGION ID(domainSourceConstructor) ENABLED START*/
+  // Add additional options here
+  /*PROTECTED REGION END*/
+}
 
-	DomainCondition::~DomainCondition()
-	{
-	}
+DomainCondition::~DomainCondition() {
+  /*PROTECTED REGION ID(domainSourceDestructor) ENABLED START*/
+  // Add additional options here
+  /*PROTECTED REGION END*/
+}
 } /* namespace alica */
