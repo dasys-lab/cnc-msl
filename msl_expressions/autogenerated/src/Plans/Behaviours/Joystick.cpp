bc35295c2b491e69b3bd9a0a422e284a4292d25e
lab-pc4 <cn@das-lab.net>
2017-08-11 17:43:53 +0200

    - add new checkbox to the joystick ui - add functionality behind this checkbox

--- msl_expressions/autogenerated/src/Plans/Behaviours/Joystick.cpp
+++ msl_expressions/autogenerated/src/Plans/Behaviours/Joystick.cpp
@@ -38,12 +38,12 @@ namespace alica
 			//if (this->smoothDrivingState)
 			//{
 			msl_actuator_msgs::MotionControl mc;
-			pastControlInput.push(std::valarray<double>(init,3));
-			pastControlInput.push(std::valarray<double>(init,3));
-			pastControlInput.push(std::valarray<double>(init,3));
-			pastTranslations.push(std::valarray<double>(init,3));
-			pastTranslations.push(std::valarray<double>(init,3));
-			pastTranslations.push(std::valarray<double>(init,3));
+			pastControlInput.push(std::valarray<double>(init, 3));
+			pastControlInput.push(std::valarray<double>(init, 3));
+			pastControlInput.push(std::valarray<double>(init, 3));
+			pastTranslations.push(std::valarray<double>(init, 3));
+			pastTranslations.push(std::valarray<double>(init, 3));
+			pastTranslations.push(std::valarray<double>(init, 3));
 			pastControlInput.pop();
 			pastControlInput.pop();
 			pastControlInput.pop();
@@ -72,29 +72,34 @@ namespace alica
 		//cout << "Joystick-Beh: " << *joy << endl;
 		if (!std::isnan(joy->motion.translation) && !std::isnan(joy->motion.rotation) && !std::isnan(joy->motion.angle))
 		{
+			double input[] = {cos(joy->motion.angle) * joy->motion.translation, sin(joy->motion.angle)
+										* joy->motion.translation,
+								joy->motion.rotation};
 
 			// smooth driving stuff
-//			if (this->smoothDrivingState)
-//			{
-			double input[] = {cos(joy->motion.angle)*joy->motion.translation, sin(joy->motion.angle)*joy->motion.translation, joy->motion.rotation};
-			pastControlInput.push(std::valarray<double>(input,3));
+			pastControlInput.push(std::valarray<double>(input, 3));
 
+			if (joy->ptControllerState == msl_msgs::JoystickCommand::PT_CONTROLLER_ON)
+			{
 
-			std::valarray<double> translation = ptController();
-			mc.motion.translation = sqrt(pow(translation[0],2.0) + pow(translation[1],2.0));
-			mc.motion.angle = atan2(translation[1],translation[0]);
-			mc.motion.rotation = translation[2];
+				std::valarray<double> translation = ptController();
+				mc.motion.translation = sqrt(pow(translation[0], 2.0) + pow(translation[1], 2.0));
+				mc.motion.angle = atan2(translation[1], translation[0]);
+				mc.motion.rotation = translation[2];
+			}
+			else
+			{
+				pastTranslations.push(std::valarray<double>(init, 3));
+				pastControlInput.pop();
+				pastTranslations.pop();
+				pastTranslations.back() = {mc.motion.translation, 0.0, 0.0};
+
+				mc.motion = joy->motion;
+			}
 
 			cout << "Joystick: x = " << mc.motion.translation << endl;
-//			} else
-//			{
-//				mc.motion = joy->motion;
-//			}
-
 			// acceleration memory
 			// saving translation of the last 3 iterations
-
-
 
 			mc.motion.angle = joy->motion.angle;
 			send(mc);
@@ -102,12 +107,12 @@ namespace alica
 		else
 		{
 			msl_actuator_msgs::MotionControl mc;
-			pastControlInput.push(std::valarray<double>(init,3));
-			pastControlInput.push(std::valarray<double>(init,3));
-			pastControlInput.push(std::valarray<double>(init,3));
-			pastTranslations.push(std::valarray<double>(init,3));
-			pastTranslations.push(std::valarray<double>(init,3));
-			pastTranslations.push(std::valarray<double>(init,3));
+			pastControlInput.push(std::valarray<double>(init, 3));
+			pastControlInput.push(std::valarray<double>(init, 3));
+			pastControlInput.push(std::valarray<double>(init, 3));
+			pastTranslations.push(std::valarray<double>(init, 3));
+			pastTranslations.push(std::valarray<double>(init, 3));
+			pastTranslations.push(std::valarray<double>(init, 3));
 			pastControlInput.pop();
 			pastControlInput.pop();
 			pastControlInput.pop();
@@ -157,13 +162,13 @@ namespace alica
 		/*PROTECTED REGION ID(initialiseParameters1421854975890) ENABLED START*/ //Add additional options here
 		if (pastTranslations.empty())
 		{
-			pastTranslations.push(std::valarray<double>(init,3) );
-			pastTranslations.push(std::valarray<double>(init,3) );
+			pastTranslations.push(std::valarray<double>(init, 3));
+			pastTranslations.push(std::valarray<double>(init, 3));
 		}
 		if (pastControlInput.empty())
 		{
-			pastControlInput.push(std::valarray<double>(init,3) );
-			pastControlInput.push(std::valarray<double>(init,3) );
+			pastControlInput.push(std::valarray<double>(init, 3));
+			pastControlInput.push(std::valarray<double>(init, 3));
 		}
 
 		lastJump = 1000;
@@ -173,13 +178,15 @@ namespace alica
 	/*PROTECTED REGION ID(methods1421854975890) ENABLED START*/ //Add additional methods here
 	std::valarray<double> Joystick::ptController()
 	{
-		int newJump = max(abs(pastControlInput.back()[0]-pastControlInput.front()[0]),abs(pastControlInput.back()[1]-pastControlInput.front()[1]));
-		if (newJump != 0) {
-			lastJump=newJump;
+		int newJump = max(abs(pastControlInput.back()[0] - pastControlInput.front()[0]),
+							abs(pastControlInput.back()[1] - pastControlInput.front()[1]));
+		if (newJump != 0)
+		{
+			lastJump = newJump;
 		}
 
 		// slope variable
-		double a = 6.33333 - 4.0/3000.0*lastJump;
+		double a = 6.33333 - 4.0 / 3000.0 * lastJump;
 		// changing point for slope
 		double b = pow(a, 2.0);
 		// sending frequency
@@ -196,20 +203,22 @@ namespace alica
 		double d1 = -2 * exp(-a * TA);
 		double d2 = exp(-2 * a * TA);
 
-		cout << "n1 = " << n1 << endl;
- 		cout << "n2 = " << n2 << endl;
-
-		cout << "d1 = " << d1 << endl;
-		cout << "d2 = " << d2 << endl;
+//		cout << "n1 = " << n1 << endl;
+//		cout << "n2 = " << n2 << endl;
+//
+//		cout << "d1 = " << d1 << endl;
+//		cout << "d2 = " << d2 << endl;
 //		cout << "d3 = " << d3 << endl;
 //		cout << "d4 = " << d4 << endl;
 
+
 //		return n1 * pastControlInput->at(0) - d1 * pastTranslations->at(0) - d2 * pastTranslations->at(1);
-		pastTranslations.push(std::valarray<double>(init,3));
-		pastTranslations.back() += n2*pastControlInput.front() - d2* pastTranslations.front();
+
+		pastTranslations.push(std::valarray<double>(init, 3));
+		pastTranslations.back() += n2 * pastControlInput.front() - d2 * pastTranslations.front();
 		pastControlInput.pop();
 		pastTranslations.pop();
-		pastTranslations.back() += n1* pastControlInput.front() - d1* pastTranslations.front();
+		pastTranslations.back() += n1 * pastControlInput.front() - d1 * pastTranslations.front();
 
 		return pastTranslations.back();
 	}
