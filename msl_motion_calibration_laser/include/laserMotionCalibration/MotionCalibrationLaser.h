ee27b1d3bff6c4329f59b9bec6d2706bff9bff3f
lab-pc4 <cn@das-lab.net>
2017-09-06 19:04:05 +0200

    added calculation of pillar center

--- msl_motion_calibration_laser/include/laserMotionCalibration/MotionCalibrationLaser.h
+++ msl_motion_calibration_laser/include/laserMotionCalibration/MotionCalibrationLaser.h
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <memory>
 #include <laserMotionCalibration/LaserPointGroup.h>
+#include <SystemConfig.h>
 
 namespace sensor_msgs
 {
@@ -16,14 +17,17 @@ namespace laserMotionCalibration
     public:
         MotionCalibrationLaser(int argc, char **argv);
         ~MotionCalibrationLaser();
-    private:
+      private:
         void onScan(const sensor_msgs::LaserScanConstPtr& laserScan);
         std::vector<std::shared_ptr<LaserPointGroup>> getThresholdGroups(const sensor_msgs::LaserScanConstPtr& laserScan, double threshold);
 
         bool initialized = false;
         ros::NodeHandle rosNode;
         ros::Subscriber subscriber;
-        std::shared_ptr<ros::Publisher> publisher;
+        std::shared_ptr<ros::Publisher> filteredPublisher;
+        std::shared_ptr<ros::Publisher> resultsPublisher;
         ros::AsyncSpinner spinner;
+        static supplementary::SystemConfig* sc;
+        static double initialIntensityThreshold;
     };
 }
--- msl_motion_calibration_laser/src/MotionCalibrationLaser.cpp
+++ msl_motion_calibration_laser/src/MotionCalibrationLaser.cpp
@@ -1,169 +1,176 @@
 #include "laserMotionCalibration/MotionCalibrationLaser.h"
 #include "ros/ros.h"
-#include <memory>
 #include <algorithm>
-//#include <sensor_msgs/PointCloud.h>
-//#include <sensor_msgs/ChannelFloat32.h>
-//#include <geometry_msgs/Point32.h>
-#include <sensor_msgs/LaserScan.h>
-#include <geometry_msgs/PoseArray.h>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/Point.h>
 #include <cmath>
 #include <ctime>
-
-const float INITIAL_INTENSITY_THRESHOLD = 0.95f;
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseArray.h>
+#include <memory>
+#include <sensor_msgs/LaserScan.h>
 
 namespace laserMotionCalibration
 {
-	using std::cout;
-	using std::endl;
-	using std::shared_ptr;
-	using std::make_shared;
+using std::cout;
+using std::endl;
+using std::shared_ptr;
+using std::make_shared;
 
-	MotionCalibrationLaser::MotionCalibrationLaser(int argc, char **argv) :
-			spinner(4)
-	{
-		spinner.start();
+double MotionCalibrationLaser::initialIntensityThreshold = -1.0;
+supplementary::SystemConfig* MotionCalibrationLaser::sc = supplementary::SystemConfig::getInstance();
 
-		subscriber = rosNode.subscribe<sensor_msgs::LaserScan>("/scan", 10, &MotionCalibrationLaser::onScan,
-																(MotionCalibrationLaser*)this);
-		this->publisher = std::make_shared < ros::Publisher
-				> (rosNode.advertise<geometry_msgs::PoseArray>("/intenseScan", 10));
-	}
+MotionCalibrationLaser::MotionCalibrationLaser(int argc, char **argv)
+    : spinner(4)
+{
+    spinner.start();
 
-	MotionCalibrationLaser::~MotionCalibrationLaser()
-	{
-		spinner.stop();
-	}
+    subscriber = rosNode.subscribe<sensor_msgs::LaserScan>("/scan", 10, &MotionCalibrationLaser::onScan, (MotionCalibrationLaser *)this);
+    this->filteredPublisher = std::make_shared<ros::Publisher>(rosNode.advertise<geometry_msgs::PoseArray>("/filteredScan", 10));
+    this->resultsPublisher = std::make_shared<ros::Publisher>(rosNode.advertise<geometry_msgs::PoseArray>("/laserResults", 10));
 
-	void MotionCalibrationLaser::onScan(const sensor_msgs::LaserScanConstPtr& laserScan)
-	{
+    initialIntensityThreshold = (*sc)["LaserLocalization"]->get<double>("LaserLocalization.initialIntensityThreshold", NULL);
+}
 
-		if (!initialized)
-		{
-			std::cout << "MIN ANGLE: " << LaserPoint::minAngle << " ANGLE ICNREMENT: " << LaserPoint::angleIncrement << std::endl;
-			std::cout << "MIN ANGLE" << laserScan->angle_min << std::endl;
-			std::cout << "MIN INCREMENT" << laserScan->angle_increment << std::endl;
-			LaserPoint::setMinAngle(laserScan->angle_min);
-			LaserPoint::setAngleIncrement(laserScan->angle_increment);
-			std::cout << "MIN ANGLE: " << LaserPoint::minAngle << " ANGLE ICNREMENT: " << LaserPoint::angleIncrement << std::endl;
-			initialized = true;
-		}
-		// choose a threshold based on max intensity
+MotionCalibrationLaser::~MotionCalibrationLaser()
+{
+    spinner.stop();
+}
 
-		auto intensities = laserScan->intensities;
-		const float maxIntensity = *max_element(intensities.begin(), intensities.end());
-		const int numOfIntensities = intensities.size();
-		std::sort(intensities.begin(), intensities.end());
+void MotionCalibrationLaser::onScan(const sensor_msgs::LaserScanConstPtr &laserScan)
+{
 
-		const float intensityThreshold = intensities[INITIAL_INTENSITY_THRESHOLD * numOfIntensities];
-		std::vector<float> filteredIntensities;
-		for (auto threshold : intensities)
-		{
-			if (threshold > intensityThreshold)
-			{
-				filteredIntensities.push_back(threshold);
-			}
-		}
+    if (!initialized)
+    {
+        std::cout << "MIN ANGLE: " << LaserPoint::minAngle << " ANGLE ICNREMENT: " << LaserPoint::angleIncrement << std::endl;
+        std::cout << "MIN ANGLE" << laserScan->angle_min << std::endl;
+        std::cout << "MIN INCREMENT" << laserScan->angle_increment << std::endl;
+        LaserPoint::setMinAngle(laserScan->angle_min);
+        LaserPoint::setAngleIncrement(laserScan->angle_increment);
+        std::cout << "MIN ANGLE: " << LaserPoint::minAngle << " ANGLE ICNREMENT: " << LaserPoint::angleIncrement << std::endl;
+        initialized = true;
+    }
+    // choose a threshold based on max intensity
+
+    auto intensities = laserScan->intensities;
+    const float maxIntensity = *max_element(intensities.begin(), intensities.end());
+    const int numOfIntensities = intensities.size();
+    std::sort(intensities.begin(), intensities.end());
+
+    const float intensityThreshold = intensities[initialIntensityThreshold * numOfIntensities];
+    std::vector<float> filteredIntensities;
+    for (auto threshold : intensities)
+    {
+        if (threshold > intensityThreshold)
+        {
+            filteredIntensities.push_back(threshold);
+        }
+    }
 #ifdef laserMotionCalibrationDebug
-		cout << "intensityThreshold: " << intensityThreshold << endl;
-		cout << "number of filtered points: " << filteredIntensities.size() << endl;
+    cout << "intensityThreshold: " << intensityThreshold << endl;
+    cout << "number of filtered points: " << filteredIntensities.size() << endl;
 #endif
 
-		const clock_t begin_time = clock();
-		std::vector<std::shared_ptr<LaserPointGroup>> pointGroups;
-		for (auto threshold : filteredIntensities)
-		{
-			pointGroups = this->getThresholdGroups(laserScan, threshold);
-			auto numberOfGroups = pointGroups.size();
+    const clock_t begin_time = clock();
+    std::vector<std::shared_ptr<LaserPointGroup>> pointGroups;
+    for (auto threshold : filteredIntensities)
+    {
+        pointGroups = this->getThresholdGroups(laserScan, threshold);
+        auto numberOfGroups = pointGroups.size();
 #ifdef laserMotionCalibrationDebug
-			cout << "t=" << threshold << " => " << numberOfGroups << " groups" << endl;
+        cout << "t=" << threshold << " => " << numberOfGroups << " groups" << endl;
 #endif
-			if (numberOfGroups == 2)
-			{
-				break;
-			}
-		}
+        if (numberOfGroups == 2)
+        {
+            break;
+        }
+    }
 
 #ifdef laserMotionCalibrationDebug
-		geometry_msgs::PoseArray poses;
-		for (auto pointGroup : pointGroups)
-		{
-			cout << pointGroup->getCenter() << "/" << pointGroup->getDistance() << "mm  ";
-			for (auto point : pointGroup->points)
-			{
-				geometry_msgs::Point rosPoint;
-				std::shared_ptr<geometry::CNPoint2D> coordinates = point.getXY();
-				rosPoint.x = coordinates->x;
-				rosPoint.y = coordinates->y;
-				geometry_msgs::Pose pose;
-				pose.position = rosPoint;
-				poses.poses.push_back(pose);
-			}
-		}
-		cout << endl;
+    geometry_msgs::PoseArray filteredPoses;
+    geometry_msgs::PoseArray resultPoses;
+    for (auto pointGroup : pointGroups)
+    {
+        geometry_msgs::Pose centerPose;
+        geometry::CNPoint2D pillarCenter = *(pointGroup->getPillarCenter());
+        centerPose.position = pillarCenter;
+        cout << pillarCenter.length() << "m  ";
+        resultPoses.poses.push_back(centerPose);
 
-		cout << clock() - begin_time << " µs" << endl;
+        for (auto point : pointGroup->points)
+        {
+            geometry_msgs::Point rosPoint;
+            std::shared_ptr<geometry::CNPoint2D> coordinates = point.getXY();
+            rosPoint.x = coordinates->x;
+            rosPoint.y = coordinates->y;
+            geometry_msgs::Pose pose;
+            pose.position = rosPoint;
+            filteredPoses.poses.push_back(pose);
+        }
+    }
+    cout << endl;
+
+    cout << clock() - begin_time << " µs" << endl;
 #endif
 
-		poses.header.frame_id = "laser"; // for debugging purposes
-		publisher->publish(poses);
-	}
+    resultPoses.header.frame_id = "laser";   // for debugging purposes
+    filteredPoses.header.frame_id = "laser"; // for debugging purposes
+    resultsPublisher->publish(resultPoses);
+    filteredPublisher->publish(filteredPoses);
+}
 
-	std::vector<std::shared_ptr<LaserPointGroup>> MotionCalibrationLaser::getThresholdGroups(const sensor_msgs::LaserScanConstPtr& laserScan, double threshold)
-	{
-		bool aboveThreshold = false;
-		int numberOfGroups = 0;
-		std::vector<std::shared_ptr<LaserPointGroup>> groups;
-		std::shared_ptr<LaserPointGroup> currentGroup;
+std::vector<std::shared_ptr<LaserPointGroup>> MotionCalibrationLaser::getThresholdGroups(const sensor_msgs::LaserScanConstPtr &laserScan, double threshold)
+{
+    bool aboveThreshold = false;
+    int numberOfGroups = 0;
+    std::vector<std::shared_ptr<LaserPointGroup>> groups;
+    std::shared_ptr<LaserPointGroup> currentGroup;
 
-		int i = 0;
-		int groupBegin = -1;
-		for (auto intensity : laserScan->intensities)
-		{
-			if (!aboveThreshold && intensity > threshold)
-			{
-				aboveThreshold = true;
-				numberOfGroups++;
-				groupBegin = i;
-				currentGroup = std::make_shared<LaserPointGroup>();
-				groups.push_back(currentGroup);
-				currentGroup->points.push_back(LaserPoint(i, intensity, laserScan->ranges[i]));
-			}
+    int i = 0;
+    int groupBegin = -1;
+    for (auto intensity : laserScan->intensities)
+    {
+        if (!aboveThreshold && intensity > threshold)
+        {
+            aboveThreshold = true;
+            numberOfGroups++;
+            groupBegin = i;
+            currentGroup = std::make_shared<LaserPointGroup>();
+            groups.push_back(currentGroup);
+            currentGroup->points.push_back(LaserPoint(i, intensity, laserScan->ranges[i]));
+        }
 
-			if (aboveThreshold && intensity < threshold)
-			{
-				aboveThreshold = false;
-				int groupEnd = i - 1;
-				//cout << "[" << numberOfGroups << "] " << groupEnd - groupBegin + 1 << " scans (" << groupBegin << "-" << groupEnd << ")" << endl;
-			}
+        if (aboveThreshold && intensity < threshold)
+        {
+            aboveThreshold = false;
+            int groupEnd = i - 1;
+            // cout << "[" << numberOfGroups << "] " << groupEnd - groupBegin + 1 << " scans (" << groupBegin << "-" << groupEnd << ")" << endl;
+        }
 
-			if (aboveThreshold)
-			{
-				//cout << "[" << numberOfGroups << "] Scan " << i << " (intensity=" << intensity << ", distance=" << laserScan->ranges[i] << ")" << endl;
-				currentGroup->points.push_back(LaserPoint(i, intensity, laserScan->ranges[i]));
-			}
+        if (aboveThreshold)
+        {
+            // cout << "[" << numberOfGroups << "] Scan " << i << " (intensity=" << intensity << ", distance=" << laserScan->ranges[i] << ")" << endl;
+            currentGroup->points.push_back(LaserPoint(i, intensity, laserScan->ranges[i]));
+        }
 
-			i++;
-		}
+        i++;
+    }
 
-		return groups;
-		// cout << "choosing threshold=" << threshold << " yielded " << numberOfGroups << " groups" << endl << endl;
-		// cout << threshold << ";" << numberOfGroups << endl;
-	}
+    return groups;
+    // cout << "choosing threshold=" << threshold << " yielded " << numberOfGroups << " groups" << endl << endl;
+    // cout << threshold << ";" << numberOfGroups << endl;
+}
+
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "motion_calibration_laser");
-	laserMotionCalibration::MotionCalibrationLaser motionCalibrationLaser =
-			laserMotionCalibration::MotionCalibrationLaser(argc, argv);
+    ros::init(argc, argv, "motion_calibration_laser");
+    laserMotionCalibration::MotionCalibrationLaser motionCalibrationLaser = laserMotionCalibration::MotionCalibrationLaser(argc, argv);
 
-	while (ros::ok())
-	{
-		ros::Duration(0.5).sleep();
-	}
+    while (ros::ok())
+    {
+        ros::Duration(0.5).sleep();
+    }
 
-	return 0;
+    return 0;
 }
