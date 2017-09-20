5e2de58268e32d9ae96bb7c42be7ea64d45ed755
lab-pc4 <cn@das-lab.net>
2017-09-20 19:23:04 +0200

    Now calculating the position of the laserscanner in the coordinate system of the two reflectors

--- msl_motion_calibration_laser/include/laserMotionCalibration/MotionCalibrationLaser.h
+++ msl_motion_calibration_laser/include/laserMotionCalibration/MotionCalibrationLaser.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include <laserMotionCalibration/LaserPointGroup.h>
 #include <SystemConfig.h>
+#include <container/CNPoint2D.h>
 
 namespace sensor_msgs
 {
@@ -19,12 +20,14 @@ namespace laserMotionCalibration
         ~MotionCalibrationLaser();
       private:
         void onScan(const sensor_msgs::LaserScanConstPtr& laserScan);
+        shared_ptr<geometry::CNPoint2D> calculateCoordinatesInPillarSystem(vector<std::shared_ptr<geometry::CNPoint2D>>& pillarCenters);
         std::vector<std::shared_ptr<LaserPointGroup>> getThresholdGroups(const sensor_msgs::LaserScanConstPtr& laserScan, double threshold);
         bool initialized = false;
         ros::NodeHandle rosNode;
         ros::Subscriber subscriber;
         std::shared_ptr<ros::Publisher> filteredPublisher;
         std::shared_ptr<ros::Publisher> resultsPublisher;
+        std::shared_ptr<ros::Publisher> positionPublisher;
         ros::AsyncSpinner spinner;
         static supplementary::SystemConfig* sc;
         static double initialIntensityThreshold;
