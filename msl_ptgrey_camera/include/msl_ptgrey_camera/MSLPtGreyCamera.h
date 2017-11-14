ff352fa55920eeee398c4cf9f44641c5118821f4
Carpe Noctem <cn@das-lab.net>
2017-11-14 17:47:30 +0100

    goalie vision

--- msl_ptgrey_camera/include/msl_ptgrey_camera/MSLPtGreyCamera.h
+++ msl_ptgrey_camera/include/msl_ptgrey_camera/MSLPtGreyCamera.h
@@ -1,16 +1,42 @@
-#ifndef MSL_PTGREY_CAMERA_MSLPTGREYCAMERA_H_
-#define MSL_PTGREY_CAMERA_MSLPTGREYCAMERA_H_
+#pragma once
+#include <flycapture/FlyCapture2.h>
+#include <iostream>
+#include <cstdint>
+#include <cstdarg>
+#include <string>
+#include <sys/types.h>
+#include <SystemConfig.h>
+#include "msl_ptgrey_camera/CamPropertySetter.h"
 
 namespace msl_ptgrey_camera
 {
+	class MSLPtGreyCamera
+	{
+	public:
+		MSLPtGreyCamera();
+		virtual ~MSLPtGreyCamera();
+		int init();
 
-class MSLPtGreyCamera
-{
-  public:
-    MSLPtGreyCamera();
-    virtual ~MSLPtGreyCamera();
-};
+        unsigned char* getNextImage() const;
+
+        void setGamma(double value);
+		void setShutter(double value);
+		void setGain(double value);
+		void setHue(double value);
+		void setSaturation(double value);
+		void setWhiteBalance(double redChannel, double blueChannel);
+        void saveCurrentImageToFile(string fpath);
+//		void imgCallback(class FlyCapture2::Image* pImage, const void* pCallbackData);
+		FlyCapture2::GigECamera* cam;
+		FlyCapture2::Image* camImg;
+		CamPropertySetter* cps;
+
+    private:
+        int imgHeight;
+        int imgWidth;
+        supplementary::SystemConfig* sc;
+        void initialiseParameters();
+
+	};
 
 } /* namespace msl_ptgrey_camera */
-
-#endif /* MSL_PTGREY_CAMERA_MSLPTGREYCAMERA_H_ */
--- msl_ptgrey_camera/src/MSLPtGreyCamera.cpp
+++ msl_ptgrey_camera/src/MSLPtGreyCamera.cpp
@@ -1,18 +1,163 @@
 #include "msl_ptgrey_camera/MSLPtGreyCamera.h"
-#include <iostream>
 
 using namespace std;
 
 namespace msl_ptgrey_camera
 {
 
-MSLPtGreyCamera::MSLPtGreyCamera()
-{
-}
+    MSLPtGreyCamera::MSLPtGreyCamera()
+    {
+        this->cam = nullptr;
+        this->camImg = nullptr;
+        this->cps = nullptr;
+    }
 
-MSLPtGreyCamera::~MSLPtGreyCamera()
-{
-    // TODO Auto-generated destructor stub
-}
+    MSLPtGreyCamera::~MSLPtGreyCamera()
+    {
+        delete this->cam;
+        delete this->camImg;
+        delete this->cps;
+    }
+
+    void PrintError(FlyCapture2::Error error);
+
+    void imgCallback(class FlyCapture2::Image *pImage, const void *pCallbackData)
+    {
+        cout << "callback!" << endl;
+    }
+
+    int MSLPtGreyCamera::init()
+    {
+
+        this->cam = new FlyCapture2::GigECamera();
+
+
+        this->camImg = new FlyCapture2::Image();
+
+        cps = new msl_ptgrey_camera::CamPropertySetter();
+
+//		FlyCapture2::GigECamera cam;
+        FlyCapture2::CameraInfo camInfo;
+        FlyCapture2::Error error;
+
+        FlyCapture2::BusManager busMgr;
+        FlyCapture2::PGRGuid guid;
+        //Format7PacketInfo fmt7PacketInfo;
+        //Format7ImageSettings fmt7ImageSettings;
+
+
+        FlyCapture2::TriggerMode triggerMode;
+        // Get Flea2 camera
+
+        busMgr.ForceAllIPAddressesAutomatically();
+
+        error = busMgr.GetCameraFromIndex(0, &guid);
+
+        if (error != FlyCapture2::PGRERROR_OK)
+        {
+            PrintError(error);
+            return -1;
+        }
+        // Connect to the camera
+
+        error = this->cam->Connect(&guid);
+
+        if (error != FlyCapture2::PGRERROR_OK)
+        {
+            PrintError(error);
+            return -1;
+        }
+
+        // Get camera information
+        error = cam->GetCameraInfo(&camInfo);
+
+        if (error != FlyCapture2::PGRERROR_OK)
+        {
+            PrintError(error);
+            return -1;
+        }
+
+        // Set Config
+        FlyCapture2::FC2Config config;
+        config.grabMode = FlyCapture2::GrabMode::DROP_FRAMES;
+        cam->SetConfiguration(&config);
+
+        // Set Imaging Mode
+        cam->SetGigEImagingMode(FlyCapture2::MODE_0);
+
+        FlyCapture2::GigEImageSettings imageSettings;
+        imageSettings.pixelFormat = FlyCapture2::PixelFormat::PIXEL_FORMAT_RAW8;
+        imageSettings.height = this->imgHeight;
+        imageSettings.width = this->imgWidth;
+        cam->SetGigEImageSettings(&imageSettings);
+
+
+        // Start Capture
+        error = cam->StartCapture();
+        if (error != FlyCapture2::PGRERROR_OK)
+        {
+            PrintError(error);
+            return -1;
+        }
+		cps->setCamera(this->cam);
+		cps->setDefaults();
+
+        return 0;
+
+    }
+
+    void MSLPtGreyCamera::setGamma(double value)
+    {
+        cps->setProperty(FlyCapture2::PropertyType::GAMMA, value);
+    }
+
+    void MSLPtGreyCamera::setShutter(double value)
+    {
+        cps->setProperty(FlyCapture2::PropertyType::SHUTTER, value);
+    }
+
+    void MSLPtGreyCamera::setGain(double value)
+    {
+        cps->setProperty(FlyCapture2::PropertyType::GAIN, value);
+    }
+
+    void MSLPtGreyCamera::setHue(double value)
+    {
+        cps->setProperty(FlyCapture2::PropertyType::HUE, value);
+    }
+
+    void MSLPtGreyCamera::setSaturation(double value)
+    {
+        cps->setProperty(FlyCapture2::PropertyType::SATURATION, value);
+    }
+
+    void MSLPtGreyCamera::setWhiteBalance(double redChannel, double blueChannel)
+    {
+        cps->setProperty(FlyCapture2::PropertyType::WHITE_BALANCE,redChannel, blueChannel);
+    }
+
+    void PrintError(FlyCapture2::Error error)
+    {
+        error.PrintErrorTrace();
+    }
+
+    unsigned char *MSLPtGreyCamera::getNextImage() const
+    {
+        cam->RetrieveBuffer(camImg);
+        return camImg->GetData();
+    }
+
+    void MSLPtGreyCamera::saveCurrentImageToFile(string fpath)
+    {
+        this->cam->RetrieveBuffer(camImg);
+        this->camImg->Save(fpath.c_str(), FlyCapture2::ImageFileFormat::RAW);
+    }
+
+    void MSLPtGreyCamera::initialiseParameters()
+    {
+        this->imgHeight = (*this->sc)["Vision2.conf"]->get<int>("Vision2.ScanLines.imgHeight", NULL);
+        this->imgWidth = (*this->sc)["Vision2.conf"]->get<int>("Vision2.ScanLines.imgWidth", NULL);
+    }
+
 
 } /* namespace msl_ptgrey_camera */
