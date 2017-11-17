2653cba19bffd53aee7684d9e6310e1816fc18dd
Stefan Jakob <jakob@vs.uni-kassel.de>
2017-11-18 00:05:12 +0100

    used right config param

--- msl_expressions/autogenerated/src/DomainBehaviour.cpp
+++ msl_expressions/autogenerated/src/DomainBehaviour.cpp
@@ -42,7 +42,7 @@ namespace alica
 		watchBallMsgPublisher = n.advertise<msl_helper_msgs::WatchBallMsg>("/WorldModel/WatchBallMsg", 10);
 		debugMsgPublisher = n.advertise<msl_helper_msgs::DebugMsg>("/DebugMsg", 10);
 
-		__maxTranslation = (*sc)["Globals"]->get<double>("Globals", "Team", sc->getHostname().c_str(), "AverageTranslation", NULL);
+		__maxTranslation = (*sc)["Drive"]->get<double>("Drive", "MaxSpeed", NULL);
 	}
 
 	DomainBehaviour::~DomainBehaviour()
