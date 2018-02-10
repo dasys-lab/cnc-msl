b150fcec71ed86e36dfde4b8fca4b932a44f6805
Carpe Noctem <cn@das-lab.net>
2018-02-10 11:21:46 +0100

    added comments

--- msl_expressions/autogenerated/src/DomainBehaviour.cpp
+++ msl_expressions/autogenerated/src/DomainBehaviour.cpp
@@ -1,47 +1,47 @@
 #include "DomainBehaviour.h"
 
-#include <RawSensorData.h>
-#include <msl_robot/kicker/Kicker.h>
-#include <SystemConfig.h>
 #include <MSLWorldModel.h>
-#include <msl_actuator_msgs/MotionControl.h>
+#include <RawSensorData.h>
+#include <SystemConfig.h>
 #include <msl_actuator_msgs/BallHandleCmd.h>
 #include <msl_actuator_msgs/KickControl.h>
+#include <msl_actuator_msgs/MotionControl.h>
 #include <msl_actuator_msgs/ShovelSelectCmd.h>
 #include <msl_helper_msgs/DebugMsg.h>
 #include <msl_helper_msgs/PassMsg.h>
 #include <msl_helper_msgs/WatchBallMsg.h>
 #include <msl_robot/MSLRobot.h>
+#include <msl_robot/kicker/Kicker.h>
 #include <msl_robot/robotmovement/RobotMovement.h>
 
 namespace alica
 {
-	DomainBehaviour::DomainBehaviour(string name) :
-			BasicBehaviour(name)
-	{
-		this->sc = supplementary::SystemConfig::getInstance();
-		this->ownID = sc->getOwnRobotID();
-		ros::NodeHandle n;
-		this->wm = msl::MSLWorldModel::get();
-		this->robot = msl::MSLRobot::get();
+DomainBehaviour::DomainBehaviour(string name)
+    : BasicBehaviour(name)
+{
+    this->sc = supplementary::SystemConfig::getInstance();
+    this->ownID = sc->getOwnRobotID();
+    ros::NodeHandle n;
+    this->wm = msl::MSLWorldModel::get();
+    this->robot = msl::MSLRobot::get();
 
-		if (wm->timeLastSimMsgReceived > 0)
-		{
-			motionControlPub = n.advertise<msl_actuator_msgs::MotionControl>(supplementary::SystemConfig::getHostname() + "/MotionControl", 10);
-			ballHandlePub = n.advertise<msl_actuator_msgs::BallHandleCmd>(supplementary::SystemConfig::getHostname() + "/BallHandleControl", 10);
-			kickControlPub = n.advertise<msl_actuator_msgs::KickControl>(supplementary::SystemConfig::getHostname() + "/KickControl", 10);
-			shovelSelectPublisher = n.advertise<msl_actuator_msgs::ShovelSelectCmd>(supplementary::SystemConfig::getHostname() + "/ShovelSelectControl", 10);
-		}
-		else
-		{
-			motionControlPub = n.advertise<msl_actuator_msgs::MotionControl>("MotionControl", 10);
-			ballHandlePub = n.advertise<msl_actuator_msgs::BallHandleCmd>("BallHandleControl", 10);
-			kickControlPub = n.advertise<msl_actuator_msgs::KickControl>("KickControl", 10);
-			shovelSelectPublisher = n.advertise<msl_actuator_msgs::ShovelSelectCmd>("ShovelSelectControl", 10);
-		}
-		passMsgPublisher = n.advertise<msl_helper_msgs::PassMsg>("WorldModel/PassMsg", 10);
-		watchBallMsgPublisher = n.advertise<msl_helper_msgs::WatchBallMsg>("/WorldModel/WatchBallMsg", 10);
-		debugMsgPublisher = n.advertise<msl_helper_msgs::DebugMsg>("/DebugMsg", 10);
+    if (wm->timeLastSimMsgReceived > 0)
+    {
+        motionControlPub = n.advertise<msl_actuator_msgs::MotionControl>(supplementary::SystemConfig::getHostname() + "/MotionControl", 10);
+        ballHandlePub = n.advertise<msl_actuator_msgs::BallHandleCmd>(supplementary::SystemConfig::getHostname() + "/BallHandleControl", 10);
+        kickControlPub = n.advertise<msl_actuator_msgs::KickControl>(supplementary::SystemConfig::getHostname() + "/KickControl", 10);
+        shovelSelectPublisher = n.advertise<msl_actuator_msgs::ShovelSelectCmd>(supplementary::SystemConfig::getHostname() + "/ShovelSelectControl", 10);
+    }
+    else
+    {
+        motionControlPub = n.advertise<msl_actuator_msgs::MotionControl>("MotionControl", 10);
+        ballHandlePub = n.advertise<msl_actuator_msgs::BallHandleCmd>("BallHandleControl", 10);
+        kickControlPub = n.advertise<msl_actuator_msgs::KickControl>("KickControl", 10);
+        shovelSelectPublisher = n.advertise<msl_actuator_msgs::ShovelSelectCmd>("ShovelSelectControl", 10);
+    }
+    passMsgPublisher = n.advertise<msl_helper_msgs::PassMsg>("WorldModel/PassMsg", 10);
+    watchBallMsgPublisher = n.advertise<msl_helper_msgs::WatchBallMsg>("/WorldModel/WatchBallMsg", 10);
+    debugMsgPublisher = n.advertise<msl_helper_msgs::DebugMsg>("/DebugMsg", 10);
 
     this->__maxTranslation = (*sc)["Drive"]->get<double>("Drive", "MaxSpeed", NULL);
     this->minRotation = (*sc)["Actuation"]->get<double>("Dribble.MinRotation", NULL);
@@ -51,19 +51,39 @@ namespace alica
     this->dribbleFactorLeft = (*sc)["Actuation"]->get<double>("Dribble.DribbleFactorLeft", NULL);
 }
 
-	DomainBehaviour::~DomainBehaviour()
-	{
-	}
+DomainBehaviour::~DomainBehaviour()
+{
+}
 
-	void alica::DomainBehaviour::send(msl_actuator_msgs::MotionControl& mc)
-	{
-//        this->wm->prediction.monitoring();
-		mc.senderID = ownID;
-		mc.timestamp = wm->getTime();
-		mc.motion.translation = min(__maxTranslation, mc.motion.translation);
-		motionControlPub.publish(mc);
-		wm->rawSensorData->processMotionControlMessage(mc);
-	}
+/**
+ * Sends a MotionControl
+ * Only use this method when using methods from robot movement
+ * If you are using a behaviour specific controller use DomainBehaviour::sendAndUpdatePT(msl_actuator_msgs::MotionControl &mc)
+ */
+void alica::DomainBehaviour::send(msl_actuator_msgs::MotionControl &mc)
+{
+    //        this->wm->prediction.monitoring();
+    mc.senderID = ownID;
+    mc.timestamp = wm->getTime();
+    mc.motion.translation = min(__maxTranslation, mc.motion.translation);
+    motionControlPub.publish(mc);
+    wm->rawSensorData->processMotionControlMessage(mc);
+}
+
+/**
+ * Sends a MotionControl and updates the central PT controller to the corresponding values
+ * Only use this method when using a behaviour specific controller in order to keep the PT controller up to date
+ * If you are using the RobotMovement use DomainBehaviour::send(msl_actuator_msgs::MotionControl &mc)
+ */
+void alica::DomainBehaviour::sendAndUpdatePT(msl_actuator_msgs::MotionControl &mc)
+{
+    mc.senderID = ownID;
+    mc.timestamp = wm->getTime();
+    mc.motion.translation = min(__maxTranslation, mc.motion.translation);
+    motionControlPub.publish(mc);
+    wm->rawSensorData->processMotionControlMessage(mc);
+    robot->robotMovement->updatePT();
+}
 
 void alica::DomainBehaviour::send(msl_actuator_msgs::BallHandleCmd &bh)
 {
@@ -72,7 +92,7 @@ void alica::DomainBehaviour::send(msl_actuator_msgs::BallHandleCmd &bh)
     // this is only for nase and his new left motor
     if (bh.rightMotor != 0 && bh.leftMotor != 0)
     {
-    	bh.rightMotor = (int)(max(this->minRotationRight, (abs(bh.rightMotor)) / this->dribbleFactorRight)) * (bh.rightMotor >= 0 ? 1 : -1);
+        bh.rightMotor = (int)(max(this->minRotationRight, (abs(bh.rightMotor)) / this->dribbleFactorRight)) * (bh.rightMotor >= 0 ? 1 : -1);
         bh.leftMotor = (int)(max(this->minRotationLeft, (abs(bh.leftMotor)) / this->dribbleFactorLeft)) * (bh.leftMotor >= 0 ? 1 : -1);
     }
     this->ballHandlePub.publish(bh);
@@ -115,20 +135,9 @@ void alica::DomainBehaviour::send(msl_helper_msgs::WatchBallMsg &wb)
     this->watchBallMsgPublisher.publish(wb);
 }
 
-	void alica::DomainBehaviour::send(msl_helper_msgs::DebugMsg& dbm)
-	{
-		dbm.senderID = ownID;
-		debugMsgPublisher.publish(dbm);
-	}
-
-	void alica::DomainBehaviour::sendAndUpdatePT(msl_actuator_msgs::MotionControl& mc)
-	{
-		mc.senderID = ownID;
-		mc.timestamp = wm->getTime();
-		mc.motion.translation = min(__maxTranslation, mc.motion.translation);
-		motionControlPub.publish(mc);
-		wm->rawSensorData->processMotionControlMessage(mc);
-		robot->robotMovement->updatePT();
-	}
+void alica::DomainBehaviour::send(msl_helper_msgs::DebugMsg &dbm)
+{
+    dbm.senderID = ownID;
+    debugMsgPublisher.publish(dbm);
+}
 } /* namespace alica */
-
