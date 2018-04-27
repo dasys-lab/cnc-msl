a23847b209310aa71af9885a9f095c7a042c660a
Marcel Hesse <marcel-hesse@hotmail.de>
2018-04-27 13:17:27 +0200

    delete old dribblecontrol

--- etc/plans/Attack/StandardAttack.pml
+++ etc/plans/Attack/StandardAttack.pml
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="ASCII"?>
 <alica:Plan xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:alica="http:///de.uni_kassel.vs.cn" id="1434046634656" name="StandardAttack" comment="lostBall" masterPlan="false" utilityFunction="" utilityThreshold="0.1" destinationPath="Plans/Attack" priority="0.0" minCardinality="1" maxCardinality="1">
   <states id="1434046634657" name="GetBall" comment="" entryPoint="1434046634658">
-    <plans xsi:type="alica:BehaviourConfiguration">../Dribble/DribbleControl.beh#1450175539163</plans>
     <plans xsi:type="alica:BehaviourConfiguration">../Behaviours/Intercept.beh#1458757193843</plans>
     <inTransitions>#1434048722503</inTransitions>
     <inTransitions>#1450175866364</inTransitions>
@@ -34,7 +33,6 @@
     <outTransitions>#1434716048579</outTransitions>
   </states>
   <states id="1450175617600" name="SideLineGetBall" comment="">
-    <plans xsi:type="alica:BehaviourConfiguration">../Dribble/DribbleControl.beh#1450175539163</plans>
     <plans xsi:type="alica:BehaviourConfiguration">FetchFromSideLine.beh#1450175679178</plans>
     <inTransitions>#1450175864724</inTransitions>
     <outTransitions>#1450175866364</outTransitions>
--- msl_expressions/autogenerated/src/Plans/Goalie/Test/GoalieBehaviours/DriveToGoal.cpp
+++ msl_expressions/autogenerated/src/Plans/Goalie/Test/GoalieBehaviours/DriveToGoal.cpp
@@ -38,55 +38,57 @@ namespace alica
         /*PROTECTED REGION ID(run1447863424939) ENABLED START*/ //Add additional options here
         auto me = wm->rawSensorData->getOwnPositionVision();
 
-        if (me == nullptr) {
+        if (me == nullptr)
+        {
             mc.motion.angle = 0;
             mc.motion.rotation = 0;
             mc.motion.translation = 0;
-			send(mc);
-			return;
+            send (mc);
+            return;
         }
 
-		auto offset = std::make_shared<geometry::CNPoint2D>(200, 0);
-		alloTarget = alloGoalMid + offset;
+        auto offset = std::make_shared < geometry::CNPoint2D > (200, 0);
+        alloTarget = alloGoalMid + offset;
 
-		// Build MostionControl using query
-		const double snapDistance = 300;
-		query->egoDestinationPoint = alloTarget->alloToEgo(*me);
-		query->egoAlignPoint = alloTarget->alloToEgo(*me);
-		query->snapDistance = snapDistance;
-		// Add goal posts as obstacles
-		auto additionalPoints = make_shared<vector<shared_ptr<geometry::CNPoint2D>>>();
-		additionalPoints->push_back(alloGoalLeft);
-		additionalPoints->push_back(alloGoalRight);
-		// Add ball as obstacle
-		auto alloBall = wm->ball->getAlloBallPosition();
-		if (alloBall != nullptr)
-			additionalPoints->push_back(alloBall);
-		query->additionalPoints = additionalPoints;
-		mc = this->robot->robotMovement->moveToPoint(query);
+        // Build MostionControl using query
+        const double snapDistance = 300;
+        query->egoDestinationPoint = alloTarget->alloToEgo(*me);
+        query->egoAlignPoint = alloTarget->alloToEgo(*me);
+        query->snapDistance = snapDistance;
+        // Add goal posts as obstacles
+        auto additionalPoints = make_shared<vector<shared_ptr<geometry::CNPoint2D>>>();
+        additionalPoints->push_back(alloGoalLeft);
+        additionalPoints->push_back(alloGoalRight);
+        // Add ball as obstacle
+        auto alloBall = wm->ball->getAlloBallPosition();
+        if (alloBall != nullptr)
+            additionalPoints->push_back(alloBall);
+        query->additionalPoints = additionalPoints;
+        mc = this->robot->robotMovement->moveToPoint(query);
 
-		// TODO: Probably remove as soon as motion is fixed
-		// Clamp translation because of motion failure
-		if (mc.motion.translation > 1500) {
-			mc.motion.translation = 1500;
-		}
+        // TODO: Probably remove as soon as motion is fixed
+        // Clamp translation because of motion failure
+        if (mc.motion.translation > 1500)
+        {
+            mc.motion.translation = 1500;
+        }
 
-		if (me->distanceTo(alloTarget) <= snapDistance)
-		{
-			mc.motion.translation = 0;
-			mc.motion.rotation = 0;
-			mc.motion.angle = 0;
-			send(mc);
-			cout << "Goal reached!" << endl;
-			this->setSuccess(true);
-			return;
-		}
-		if (std::isnan(mc.motion.translation))
-		{
-			return;
-		}
-		// cout << "DriveToGoal: Distance left: " << me->distanceTo(alloTarget) << endl;
-		send (mc);
+        if (me->distanceTo(alloTarget) <= snapDistance)
+        {
+            mc.motion.translation = 0;
+            mc.motion.rotation = 0;
+            mc.motion.angle = 0;
+            send (mc);
+            cout << "Goal reached!" << endl;
+            this->setSuccess(true);
+            return;
+        }
+        if (std::isnan(mc.motion.translation))
+        {
+            return;
+        }
+        // cout << "DriveToGoal: Distance left: " << me->distanceTo(alloTarget) << endl;
+        send (mc);
         /*PROTECTED REGION END*/
     }
     void DriveToGoal::initialiseParameters()
@@ -94,28 +96,28 @@ namespace alica
         /*PROTECTED REGION ID(initialiseParameters1447863424939) ENABLED START*/ //Add additional options here
         /*PROTECTED REGION END*/
     }
-
-/*PROTECTED REGION ID(methods1447863424939) ENABLED START*/ //Add additional methods here
+    /*PROTECTED REGION ID(methods1447863424939) ENABLED START*/ //Add additional methods here
     void DriveToGoal::updateGoalPosition()
     {
-    	shared_ptr<geometry::CNPoint2D> laserDetectedEgoGoalMid =  wm->rawSensorData->getEgoGoalMid();
+        shared_ptr < geometry::CNPoint2D > laserDetectedEgoGoalMid = wm->rawSensorData->getEgoGoalMid();
 
         if (laserDetectedEgoGoalMid)
         {
-        	alloGoalMid = laserDetectedEgoGoalMid;
+            alloGoalMid = laserDetectedEgoGoalMid;
         }
         else
         {
-        	alloGoalMid = wm->field->posOwnGoalMid();
+            alloGoalMid = wm->field->posOwnGoalMid();
         }
-	if (alloGoalMid == nullptr || wm->field->posLeftOwnGoalPost() == nullptr)  {
-		cout << "Can't determine goal mid using scanner, alloGoalMid == nullptr" << endl;
-		return;
-	}
-    	alloGoalLeft = make_shared < geometry::CNPoint2D
-    	                > (alloGoalMid->x, wm->field->posLeftOwnGoalPost()->y - goalieSize / 2);
-    	alloGoalRight = make_shared < geometry::CNPoint2D
-    	                > (alloGoalMid->x, wm->field->posRightOwnGoalPost()->y + goalieSize / 2);
+        if (alloGoalMid == nullptr || wm->field->posLeftOwnGoalPost() == nullptr)
+        {
+            cout << "Can't determine goal mid using scanner, alloGoalMid == nullptr" << endl;
+            return;
+        }
+        alloGoalLeft = make_shared < geometry::CNPoint2D
+                > (alloGoalMid->x, wm->field->posLeftOwnGoalPost()->y - goalieSize / 2);
+        alloGoalRight = make_shared < geometry::CNPoint2D
+                > (alloGoalMid->x, wm->field->posRightOwnGoalPost()->y + goalieSize / 2);
     }
 /*PROTECTED REGION END*/
 } /* namespace alica */
